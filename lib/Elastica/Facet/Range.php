d5a019045c7c95ecd84f3d6cf5ea15a6adbb9c19
Jasper van Wanrooy <jasper@vanwanrooy.net>
2011-04-17 21:37:52 +0200

    Check for validity of fields upon converting to an array instead of switching values. Checked the facet code with phpcs and applied required changes.

--- lib/Elastica/Facet/Range.php
+++ lib/Elastica/Facet/Range.php
@@ -20,40 +20,23 @@ class Elastica_Facet_Range extends Elastica_Facet_Abstract
 	/**
 	 * Sets the field for the range.
 	 * 
-	 * @param string $field
+	 * @param string $field The name of the field for range.
 	 * @return Elastica_Facet_Range
 	 */
 	public function setField($field) {
-		/**
-		 * Unset the fields by their separates.
-		 */
-		$this->unsetParam('key_field')
-		     ->unsetParam('value_field');
-		
-		/**
-		 * Set the field name.
-		 */
 		return $this->setParam('field', $field);
 	}
 	
 	/**
 	 * Sets the fields by their separate key and value fields.
 	 * 
-	 * @param string $keyField
-	 * @param string $valueField
+	 * @param string $keyField The key_field param for the range. 
+	 * @param string $valueField The key_value param for the range.
 	 * @return Elastica_Facet_Range
 	 */
-	public function setKeyValueField($keyField, $valueField) {
-		/**
-		 * Only one of the types can be defined, reset the others.
-		 */
-		$this->unsetParam('field');
-		
-		/**
-		 * Set the key field and value field.
-		 */
+	public function setKeyValueFields($keyField, $valueField) {
 		return $this->setParam('key_field', $keyField)
-		            ->setParam('value_field', $valueField);
+					->setParam('value_field', $valueField);
 	}
 	
 	/**
@@ -75,8 +58,8 @@ class Elastica_Facet_Range extends Elastica_Facet_Abstract
 	/**
 	 * Adds a range to the range facet.
 	 * 
-	 * @param mixed $from
-	 * @param mixed $to
+	 * @param mixed $from The from for the range.
+	 * @param mixed $to The to for the range.
 	 * @return Elastica_Facet_Range
 	 */
 	public function addRange($from = null, $to = null) {
@@ -91,7 +74,7 @@ class Elastica_Facet_Range extends Elastica_Facet_Abstract
 	/**
 	 * Gets a general parameter for this facet by key.
 	 * 
-	 * @param $key
+	 * @param string $key The key of the param to fetch.
 	 * @return mixed
 	 */
 	public function getParam($key) {
@@ -113,19 +96,6 @@ class Elastica_Facet_Range extends Elastica_Facet_Abstract
 	}
 	
 	/**
-	 * Unsets a parameter for this facet if it is set.
-	 * 
-	 * @param string $key
-	 * @return Elastica_Facet_Range
-	 */
-	public function unsetParam($key) {
-		if (isset($this->_params[$key])) {
-			unset($this->_params[$key]);
-		}
-		return $this;
-	}
-	
-	/**
 	 * Creates the full facet definition, which includes the basic
 	 * facet definition of the parent.
 	 * 
@@ -133,6 +103,21 @@ class Elastica_Facet_Range extends Elastica_Facet_Abstract
 	 * @return array
 	 */
 	public function toArray() {
+		/**
+		 * Check the facet for validity:
+		 *  - only the field param OR the key_field should be set.
+		 */
+		if (!isset($this->_params['field']) && !isset($this->_params['key_field'])) {
+			throw new Elastica_Exception_Invalid('Neither field nor key_field is set.');
+		}
+		
+		if (isset($this->_params['field']) && $this->_params['key_field']) {
+			throw new Elastica_Exception_Invalid('Either field or key_field and key_value should be set.');
+		}
+		
+		/**
+		 * Set the range in the abstract as param.
+		 */
 		parent::setParam('range', $this->_params);
 		return $this->_query;
 	}
