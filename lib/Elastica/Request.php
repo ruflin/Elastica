71b92ede4bf93dcf8532acd961a2b2fa46042745
Nicolas Ruflin <spam@ruflin.com>
2011-06-19 00:38:31 +0200

    move request stuff to transport. memcache transport added. not working yet...

--- lib/Elastica/Request.php
+++ lib/Elastica/Request.php
@@ -13,8 +13,8 @@ class Elastica_Request {
 	const GET = 'GET';
 	const DELETE = 'DELETE';
 
+	protected $_client;
 	protected $_path;
-	// TODO: set default method?
 	protected $_method;
 	protected $_data;
 
@@ -23,7 +23,8 @@ class Elastica_Request {
 	 * @param string $method Request method (use const's)
 	 * @param array $data Data array
 	 */
-	public function __construct($path, $method, $data = array()) {
+	public function __construct(Elastica_Client $client, $path, $method, $data = array()) {
+		$this->_client = $client;
 		$this->_path = $path;
 		$this->_method = $method;
 		$this->_data = $data;
@@ -80,5 +81,42 @@ class Elastica_Request {
 	 */
 	public function getPath() {
 		return $this->_path;
+	}
+
+	public function getClient() {
+		return $this->_client;
+	}
+
+	/**
+	 * Returns a specific config key or the whole
+	 * config array if not set
+	 *
+	 * @param string $key Config key
+	 * @return array|string Config value
+	 */
+	public function getConfig($key = '') {
+		return $this->getClient()->getConfig($key);
+	}
+
+	/**
+	 * Returns an instance of the transport type
+	 *
+	 * @return Elastica_Transport_Abstract Transport object
+	 * @throws Elastica_Exception_Invalid If invalid transport type
+	 */
+	public function getTransport() {
+		$className = 'Elastica_Transport_' . $this->_client->getConfig('transport');
+		if (!class_exists($className)) {
+			throw new Elastica_Exception_Invalid('Invalid transport');
+		}
+
+		return new $className($this);
+	}
+
+	public function send() {
+		$transport = $this->getTransport();
+
+		// TODO Implement multiple servers
+		return $transport->exec($this->getClient()->getHost(), $this->getClient()->getPort());
 	}
 }
