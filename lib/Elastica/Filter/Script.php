1bba1886c994778cee98698cf8aa08ed3301c0b7
Denis Fakhrtdinov <me@shizz.ru>
2011-06-28 13:49:25 +0400

    Filter script

---
+++ lib/Elastica/Filter/Script.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Elastica_Filter_Script extends Elastica_Filter_Abstract {
+
+    /**
+     * @var array|Elastica_Query_Abstract 
+     */
+    protected $_query = null; 
+
+    public function __construct($query = null) { 
+        if(! is_null($query)) {
+            $this->setQuery($query);
+        } 
+    } 
+
+    public function setQuery($query) {
+        if(! $query instanceof Elastica_Query_Abstract && ! is_array($query)) {
+            throw new InvalidArgumentException('expected an array or instance of Elastica_Query_Abstract'); 
+        } 
+        $this->_query = $query; 
+        return $this;
+    } 
+
+    public function toArray() {
+        return array(
+            'script' => ( 
+                $this->_query instanceof Elastica_Query_Abstract 
+                ? $this->_query->toArray()
+                : $this->_query
+            ),
+        );
+    } 
+
+} 
