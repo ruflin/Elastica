1fc5f1e4f67fafc472386361332539a222935779
Nicolas Ruflin <spam@ruflin.com>
2011-08-07 12:03:24 +0200

    MoreLikeThis query merged and fixed

--- lib/Elastica/Query/MoreLikeThis.php
+++ lib/Elastica/Query/MoreLikeThis.php
@@ -10,36 +10,36 @@
  */
 class Elastica_Query_MoreLikeThis extends Elastica_Query_Abstract
 {
-    /**
-     * @var array
-     */
+	/**
+	 * @var array
+	 */
 	protected $_fields = array();
 
-    /**
-     * @var string
-     */
+	/**
+	 * @var string
+	 */
 	protected $_likeText   = null;
-    
-    protected $_percTermsToMatch = 0.3;
-    protected $_minTermFreq = 2;
+
+	protected $_percTermsToMatch = 0.3;
+	protected $_minTermFreq = 2;
 	protected $_maxQueryTerms = 25;
-    protected $_minDocFreq = 5;
-    protected $_maxDocFreq = null;
-    protected $_minWordLen = 0;
-    protected $_maxWordLen = null;
-    protected $_boostTerms = 1;
+	protected $_minDocFreq = 5;
+	protected $_maxDocFreq = null;
+	protected $_minWordLen = 0;
+	protected $_maxWordLen = null;
+	protected $_boostTerms = 1;
 	protected $_boost = 1.0;
 
-    /**
-     * @var string
-     */
-    protected $_analyzer = null;
-    
-    /**
-     * @var array
-     */
-    protected $_stopWords = null;
-    
+	/**
+	 * @var string
+	 */
+	protected $_analyzer = null;
+
+	/**
+	 * @var array
+	 */
+	protected $_stopWords = null;
+
 
 	/**
 	 * Adds field to flt query
@@ -85,120 +85,119 @@ class Elastica_Query_MoreLikeThis extends Elastica_Query_Abstract
 	}
 
 
-    /**
-     * @param float $perc    percentage
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setPercentTermsToMatch( $perc ) {
-        $perc= (float)$perc;
+	/**
+	 * @param float $perc percentage
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setPercentTermsToMatch( $perc ) {
+		$perc= (float) $perc;
 
-        $this->_percTermsToMatch = $perc;
+		$this->_percTermsToMatch = $perc;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @param int $value
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setMinTermFrequency( $value ) {
-        $value = (int)$value;
-        if ($value < 0) {
-            $value = 0;
-        }
+	/**
+	 * @param int $value
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setMinTermFrequency( $value ) {
+		$value = (int)$value;
+		if ($value < 0) {
+			$value = 0;
+		}
 
-        $this->_minTermFreq = $value;
+		$this->_minTermFreq = $value;
 
-        return $this;
-    }
-
-
-    /**
-     * @param int $value
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setMinDocFrequency( $value ) {
-        $value = (int)$value;
-        $value = ($value < 0) ?  5 : $value;
-
-        $this->_minDocFreq = $value;
-        return $this;
-    }
-
-    /**
-     * @param int $value
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setMaxDocFrequency($value) {
-        $value = (int)$value;
-        $value = ($value < 0) ?  null : $value;
-
-        $this->_maxDocFreq = $value;
-        return $this;
-    }
+		return $this;
+	}
 
 
-    /**
-     * @param int $value
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setMinWordLength( $value ) {
-        $value = (int)$value;
-        $value = ($value <= 0) ?  0 : $value;
+	/**
+	 * @param int $value
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setMinDocFrequency( $value ) {
+		$value = (int)$value;
+		$value = ($value < 0) ?  5 : $value;
 
-        $this->_minWordLen = $value;
-        return $this;
-    }
+		$this->_minDocFreq = $value;
+		return $this;
+	}
 
-    /**
-     * @param int $value
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setMaxWordLength($value) {
-        $value = (int)$value;
-        $value = ($value < 0) ?  null : $value;
+	/**
+	 * @param int $value
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setMaxDocFrequency($value) {
+		$value = (int)$value;
+		$value = ($value < 0) ?  null : $value;
 
-        $this->_maxWordLen = $value;
-        return $this;
-    }
-
-
-    /**
-     *
-     * @param int $value;
-     * @link http://www.elasticsearch.org/guide/reference/query-dsl/mlt-query.html
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setBoostTerms($value) {
-        $value = (int)$value;
-        $value = ($value < 0) ? 1 : $value;
-        $this->_boostTerms = $value;
-        return $this;
-    }
+		$this->_maxDocFreq = $value;
+		return $this;
+	}
 
 
-    /**
-     * @param string $value
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setAnalyzer( $value ) {
-        $value = trim($value);
-        if (!empty($value)) {
-            $this->_analyzer = $value;
-        }
+	/**
+	 * @param int $value
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setMinWordLength( $value ) {
+		$value = (int)$value;
+		$value = ($value <= 0) ?  0 : $value;
 
-        return $this;
-    }
+		$this->_minWordLen = $value;
+		return $this;
+	}
+
+	/**
+	 * @param int $value
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setMaxWordLength($value) {
+		$value = (int)$value;
+		$value = ($value < 0) ?  null : $value;
+
+		$this->_maxWordLen = $value;
+		return $this;
+	}
 
 
-    /**
-     * @param Array $words
-     * @return Elastica_Query_MoreLikeThis
-     */
-    public function setStopWords(Array $words) {
-        $this->_stopWords = $words;
-        return $this;
-    }
+	/**
+	 * @param int $value
+	 * @return Elastica_Query_MoreLikeThis
+	 * @link http://www.elasticsearch.org/guide/reference/query-dsl/mlt-query.html
+	 */
+	public function setBoostTerms($value) {
+		$value = (int) $value;
+		$value = ($value < 0) ? 1 : $value;
+		$this->_boostTerms = $value;
+		return $this;
+	}
+
+
+	/**
+	 * @param string $value
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setAnalyzer( $value ) {
+		$value = trim($value);
+		if (!empty($value)) {
+			$this->_analyzer = $value;
+		}
+
+		return $this;
+	}
+
+
+	/**
+	 * @param Array $words
+	 * @return Elastica_Query_MoreLikeThis
+	 */
+	public function setStopWords(Array $words) {
+		$this->_stopWords = $words;
+		return $this;
+	}
 
 	/**
 	 * Converts more_like_this  query to array
@@ -209,7 +208,7 @@ class Elastica_Query_MoreLikeThis extends Elastica_Query_Abstract
 	public function toArray() {
 
 		if (!empty($this->_fields)) {
-			$args['fields'] = $this->_fields;
+			$args['mlt_fields'] = $this->_fields;
 		}
 
 		if (!empty($this->_boost)) {
@@ -222,28 +221,28 @@ class Elastica_Query_MoreLikeThis extends Elastica_Query_Abstract
 
 		$args['max_query_terms'] = $this->_maxQueryTerms;
 
-        $args['percent_terms_to_match']   = $this->_percTermsToMatch;
-        $args['min_term_freq'] = $this->_minTermFreq;
+		$args['percent_terms_to_match']   = $this->_percTermsToMatch;
+		$args['min_term_freq'] = $this->_minTermFreq;
 
-        if (!empty($this->_stopWords)) {
-            $args['stop_words'] = $this->_stopWords;
-        }
+		if (!empty($this->_stopWords)) {
+			$args['stop_words'] = $this->_stopWords;
+		}
 
-        if (!empty($this->_analyzer)) {
-            $args['analyzer'] = $this->_analyzer;
-        }
+		if (!empty($this->_analyzer)) {
+			$args['analyzer'] = $this->_analyzer;
+		}
 
-        $args['min_doc_freq'] = $this->_minDocFreq;
+		$args['min_doc_freq'] = $this->_minDocFreq;
 
-        if ($this->_maxDocFreq != null) {
-            $args['max_doc_freq'] = $this->_maxDocFreq;
-        }
+		if ($this->_maxDocFreq != null) {
+			$args['max_doc_freq'] = $this->_maxDocFreq;
+		}
 
-        $args['min_word_len'] = $this->_minWordLen;
-        if ($this->_maxWordLen != null) {
-            $args['max_word_len'] = $this->_maxWordLen;
-        }
-        $args['boost_terms']  = $this->_boostTerms;
+		$args['min_word_len'] = $this->_minWordLen;
+		if ($this->_maxWordLen != null) {
+			$args['max_word_len'] = $this->_maxWordLen;
+		}
+		$args['boost_terms']  = $this->_boostTerms;
 
 		return array('mlt' => $args);
 	}
--- test/lib/Elastica/Query/MoreLikeThisTest.php
+++ test/lib/Elastica/Query/MoreLikeThisTest.php
@@ -18,37 +18,37 @@ class Elastica_Query_MoreLikeThisTest extends PHPUnit_Framework_TestCase
 		//$index->getSettings()->setNumberOfShards(1);
 
 		$type = new Elastica_Type($index, 'helloworldmlt');
-        $mapping = new Elastica_Type_Mapping($type , array(
-               'email' => array('store' => 'yes', 'type' => 'string', 'index' => 'analyzed'),
-               'content' => array('store' => 'yes', 'type' => 'string',  'index' => 'analyzed'),
-          ));
+		$mapping = new Elastica_Type_Mapping($type , array(
+			   'email' => array('store' => 'yes', 'type' => 'string', 'index' => 'analyzed'),
+			   'content' => array('store' => 'yes', 'type' => 'string',  'index' => 'analyzed'),
+		  ));
 
-        $mapping->setSource(array('enabled' => false));
-        $type->setMapping($mapping);
-        
+		$mapping->setSource(array('enabled' => false));
+		$type->setMapping($mapping);
+
 
 		$doc = new Elastica_Document(1000, array('email' => 'testemail@gmail.com', 'content' => 'This is a sample post. Hello World Fuzzy Like This!'));
 		$type->addDocument($doc);
 
-        $doc = new Elastica_Document(1001, array('email' => 'nospam@gmail.com', 'content' => 'This is a fake nospam email address for gmail'));
-        $type->addDocument($doc);
+		$doc = new Elastica_Document(1001, array('email' => 'nospam@gmail.com', 'content' => 'This is a fake nospam email address for gmail'));
+		$type->addDocument($doc);
 
 		// Refresh index
 		$index->refresh();
 
 		$mltQuery = new Elastica_Query_MoreLikeThis();
-        $mltQuery->setLikeText("fake gmail sample");
-        $mltQuery->addFields(array("email","content"));
-        $mltQuery->setMaxQueryTerms(1);
-        $mltQuery->setMinDocFrequency(1);
-        $mltQuery->setMinTermFrequency(1);
+		$mltQuery->setLikeText("fake gmail sample");
+		$mltQuery->addFields(array("email","content"));
+		$mltQuery->setMaxQueryTerms(1);
+		$mltQuery->setMinDocFrequency(1);
+		$mltQuery->setMinTermFrequency(1);
 
-        $query = new Elastica_Query();
-        $query->setFields(array("email", "content"));
-        $query->setQuery($mltQuery);
+		$query = new Elastica_Query();
+		$query->setFields(array("email", "content"));
+		$query->setQuery($mltQuery);
 
 		$resultSet = $type->search($query);
-        $resultSet->getResponse()->getData();
+		$resultSet->getResponse()->getData();
 		$this->assertEquals(2, $resultSet->count());
 	}
 }
