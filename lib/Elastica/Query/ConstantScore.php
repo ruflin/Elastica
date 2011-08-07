f28fa36695481037d5076cf4ce07fb44ed396efc
Nicolas Ruflin <spam@ruflin.com>
2011-08-07 21:43:03 +0200

    Elastica_Query_ConstantScore improved and fixed

--- lib/Elastica/Query/ConstantScore.php
+++ lib/Elastica/Query/ConstantScore.php
@@ -9,49 +9,25 @@
  * @author Nicolas Ruflin <spam@ruflin.com>
  * @link http://www.elasticsearch.org/guide/reference/query-dsl/constant-score-query.html
  */
-class Elastica_Query_ConstantScore extends Elastica_Query_Abstract {
-
-	protected $_boost = null;
-
-	/**
-	 * @var array|Elastica_Filter_Abstract
-	 */
-	protected $_filter = null;
-
-
+class Elastica_Query_ConstantScore extends Elastica_Query_Abstract
+{
 	/**
 	 * @param array|Elastica_Filter_Abstract $filter
 	 * @return Elastica_Query_ConstantScore Query object
-	 * @throws InvalidArgumentException
 	 */
 	public function setFilter($filter) {
-		if (!is_array($filter) && !$filter instanceof Elastica_Filter_Abstract) {
-			throw new InvalidArgumentException('expected an array or Elastica_Filter_Abstract');
+		if ($filter instanceof Elastica_Filter_Abstract) {
+			$filter = $filter->toArray();
 		}
-		$this->_filter = $filter;
-		return $this;
+		return $this->setParam('filter', $filter);
 	}
 
 	/**
-	 * @return array Query array
-	 * @see Elastica_Query_Abstract::toArray()
+	 * @param float $boost
+	 * @return Elastica_Query_ConstantScore
 	 */
-	public function toArray() {
-		$ret = array(
-			'constant_score' => array(
-				'filter' => (
-					$this->_filter instanceof Elastica_Filter_Abstract
-					? $this->_filter->toArray()
-					: $this->_filter
-				),
-			)
-		);
-
-		if (! is_null($this->_boost)) {
-			$ret['boost'] = $this->_boost;
-		}
-
-		return $ret;
+	public function setBoost($boost) {
+		return $this->setParam('boost', $boost);
 	}
 }
 
