9b9f4d4265b19ee9124f50b4c8065723474e5d11
Nicolas Ruflin <spam@ruflin.com>
2011-08-25 15:54:18 +0200

    https transport added

---
+++ lib/Elastica/Transport/Https.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Elastica Http Transport object
+ *
+ * @category Xodoa
+ * @package Elastica
+ * @author Nicolas Ruflin <spam@ruflin.com>
+ */
+class Elastica_Transport_Https extends Elastica_Transport_Http {
+
+	/**
+	 * @var string https scheme
+	 */
+	protected $_scheme = 'https';
+	
+	/**
+	 * Overloads setupCurl to set SSL params
+	 */
+	protected function _setupCurl($connection) {
+		parent::_setupCurl($connection);
+		curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
+	}
+}
