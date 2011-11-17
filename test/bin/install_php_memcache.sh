89c6e01f4d197260a5eae33a42698f20649e5f3b
Fabian Vogler <fabian@equivalence.ch>
2011-11-17 14:52:31 +0100

    Install memcache extension

--- test/bin/install_php_memcache.sh
+++ test/bin/install_php_memcache.sh
@@ -3,5 +3,5 @@
 # Build and install PHP Memcache extension
 wget http://pecl.php.net/get/memcache-2.2.6.tgz
 tar -xzf memcache-2.2.6.tgz
-sh -c "cd memcache-2.2.6 && phpize && ./configure --enable-memcache && make"
+sh -c "cd memcache-2.2.6 && phpize && ./configure --enable-memcache && make && sudo make install"
 echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
