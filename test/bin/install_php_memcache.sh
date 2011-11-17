4afe6da331205618b7ea25b32b02f697acb65a95
Fabian Vogler <fabian@equivalence.ch>
2011-11-17 14:45:30 +0100

    Install Pake

--- test/bin/install_php_memcache.sh
+++ test/bin/install_php_memcache.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install Pake
+pyrus channel-discover pear.indeyets.ru
+pyrus install -f http://pear.indeyets.ru/get/pake-1.6.3.tgz
+
+# Build and install PHP Memcache extension
 wget http://pecl.php.net/get/memcache-2.2.6.tgz
 tar -xzf memcache-2.2.6.tgz
 sh -c "cd memcache-2.2.6 && php `pyrus get php_dir|tail -1`/pake.php install"
