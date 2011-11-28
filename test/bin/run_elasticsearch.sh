a5234a23328aa97740f938b827af06157f14b4e3
Fabian Vogler <fabian@equivalence.ch>
2011-11-28 21:25:19 +0100

    Run elastic search with -server

--- test/bin/run_elasticsearch.sh
+++ test/bin/run_elasticsearch.sh
@@ -4,8 +4,7 @@ wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.18
 tar -xzf elasticsearch-0.18.4.tar.gz
 elasticsearch-0.18.4/bin/plugin install mapper-attachments
 
-JAVA_OPTS="-server"
-export JAVA_OPT
+export JAVA_OPTS="-server"
 elasticsearch-0.18.4/bin/elasticsearch &
 
 echo "Waiting until elasticsearch is ready on port 9200"
