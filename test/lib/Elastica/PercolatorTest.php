612cc8218a50747bbc56815fb288d0a7b732640c
Nicolas Ruflin <spam@ruflin.com>
2011-12-06 20:57:37 +0100

    Elastica_Percolator and tests added

---
+++ test/lib/Elastica/PercolatorTest.php
@@ -0,0 +1,52 @@
+<?php
+require_once dirname(__FILE__) . '/../../bootstrap.php';
+
+
+class Elastica_PercolatorTest extends Elastica_Test
+{
+	public function testConstruct() {
+		$index = $this->_createIndex();
+		$percolator = new Elastica_Percolator($index);
+		
+		$percolatorName = 'percotest';
+		
+		$query = new Elastica_Query_Term(array('field1' => 'value1'));
+		$response = $percolator->registerQuery($percolatorName, $query);
+		
+		$data = $response->getData();
+		
+		$expectedArray = array(
+			'ok' => true,
+			'_type' => $index->getName(),
+			'_index' => '_percolator',
+			'_id' => $percolatorName,
+			'_version' => 1
+		);
+
+		$this->assertEquals($expectedArray, $data);
+	}
+	
+	public function testMatchDoc() {
+		$index = $this->_createIndex();
+		$percolator = new Elastica_Percolator($index);
+		
+		$percolatorName = 'percotest';
+		
+		$query = new Elastica_Query_Term(array('name' => 'ruflin'));
+		$response = $percolator->registerQuery($percolatorName, $query);
+		
+		$doc1 = new Elastica_Document();
+		$doc1->add('name', 'ruflin');
+
+		$doc2 = new Elastica_Document();
+		$doc2->add('name', 'nicolas');
+		
+		$matches1 = $percolator->matchDoc($doc1);
+		
+		$this->assertTrue(in_array($percolatorName, $matches1));
+		$this->assertEquals(1, count($matches1));
+		
+		$matches2 = $percolator->matchDoc($doc2);
+		$this->assertEmpty($matches2);
+	}
+}
