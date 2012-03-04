6de7bac4350c02c5638a81aefd6b85c34283c55d
Fabian Vogler <fabian@equivalence.ch>
2012-03-04 15:41:35 +0100

    Updated to elasticsearch 0.19

--- test/bin/run_elasticsearch.sh
+++ test/bin/run_elasticsearch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.18.5.tar.gz
-tar -xzf elasticsearch-0.18.5.tar.gz
-sed 's/# index.number_of_shards: 1/index.number_of_shards: 1/' elasticsearch-0.18.5/config/elasticsearch.yml > elasticsearch-0.18.5/config/elasticsearch.yml
-sed 's/# index.number_of_replicas: 0/index.number_of_replicas: 0/' elasticsearch-0.18.5/config/elasticsearch.yml > elasticsearch-0.18.5/config/elasticsearch.yml
-sed 's/# discovery.zen.ping.multicast.enabled: false/discovery.zen.ping.multicast.enabled: false/' elasticsearch-0.18.5/config/elasticsearch.yml > elasticsearch-0.18.5/config/elasticsearch.yml
-elasticsearch-0.18.5/bin/plugin install mapper-attachments
+wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.0.tar.gz
+tar -xzf elasticsearch-0.19.0.tar.gz
+sed 's/# index.number_of_shards: 1/index.number_of_shards: 1/' elasticsearch-0.19.0/config/elasticsearch.yml > elasticsearch-0.19.0/config/elasticsearch.yml
+sed 's/# index.number_of_replicas: 0/index.number_of_replicas: 0/' elasticsearch-0.19.0/config/elasticsearch.yml > elasticsearch-0.19.0/config/elasticsearch.yml
+sed 's/# discovery.zen.ping.multicast.enabled: false/discovery.zen.ping.multicast.enabled: false/' elasticsearch-0.19.0/config/elasticsearch.yml > elasticsearch-0.19.0/config/elasticsearch.yml
+elasticsearch-0.19.0/bin/plugin -install elasticsearch/elasticsearch-mapper-attachments/1.2.0
 
 export JAVA_OPTS="-server"
-elasticsearch-0.18.5/bin/elasticsearch &
+elasticsearch-0.19.0/bin/elasticsearch &
 
 echo "Waiting until elasticsearch is ready on port 9200"
 while [[ -z `curl -s 'http://localhost:9200' ` ]]
