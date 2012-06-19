829cda7fe1d70c976b4312ea4b959bc8e29ffb3a
Francisco Facioni <fran6co@gmail.com>
2012-06-19 13:38:51 -0300

    fixes count when using filter queries

--- lib/Elastica/Type.php
+++ lib/Elastica/Type.php
@@ -178,10 +178,12 @@ class Elastica_Type implements Elastica_Searchable {
 	 */
 	public function count($query = '') {
 		$query = Elastica_Query::create($query);
-		$path = '_count';
+		$path = '_search?search_type=count';
 
-		$data = $this->request($path, Elastica_Request::GET, $query->getQuery())->getData();
-		return (int) $data['count'];
+		$response = $this->request($path, Elastica_Request::GET, $query->toArray());
+		$resultSet = new Elastica_ResultSet($response);
+
+		return $resultSet->getTotalHits();
 	}
 
 	/**
