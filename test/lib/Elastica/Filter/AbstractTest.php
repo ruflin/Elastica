575d2027fa64541460e6deecf6f8a2aa64f91e09
Pierre Durand <pierre@plyce.com>
2012-06-23 17:20:22 +0200

    Improve Elastica_Filter_Abstract::setCached() Add Elastica_Filter_Abstract::setCacheKey() Add tests for Elastica_Filter_Abstract

---
+++ test/lib/Elastica/Filter/AbstractTest.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../bootstrap.php';
+
+
+class Elastica_Filter_AbstractTest extends PHPUnit_Framework_TestCase {
+	public function testSetCached() {
+		$stubFilter = $this->getStub();
+		
+		$stubFilter->setCached(true);
+		$arrayFilter = $stubFilter->toArray();
+		$this->assertTrue($arrayFilter['_cache']);
+		
+		$stubFilter->setCached(false);
+		$arrayFilter = $stubFilter->toArray();
+		$this->assertFalse($arrayFilter['_cache']);
+	}
+	
+	public function testSetCachedDefaultValue() {
+		$stubFilter = $this->getStub();
+		
+		$stubFilter->setCached();
+		$arrayFilter = $stubFilter->toArray();
+		$this->assertTrue($arrayFilter['_cache']);
+	}
+	
+	public function testSetCacheKey() {
+		$stubFilter = $this->getStub();
+		
+		$cacheKey = 'myCacheKey';
+		
+		$stubFilter->setCacheKey($cacheKey);
+		$arrayFilter = $stubFilter->toArray();
+		$this->assertEquals($cacheKey, $arrayFilter['_cache_key']);
+	}
+	
+	/**
+	 * @expectedException Elastica_Exception_Invalid
+	 */
+	public function testSetCacheKeyEmptyKey() {
+		$stubFilter = $this->getStub();
+		
+		$cacheKey = '';
+		
+		$stubFilter->setCacheKey($cacheKey);
+	}
+	
+	private function getStub() {
+		return $this->getMockForAbstractClass('Elastica_Filter_Abstract');
+	}
+}
