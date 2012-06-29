7cc1bf2ac0a62686c620b740c300b9b4ac039853
Pierre Durand <pierre@plyce.com>
2012-06-29 16:23:32 +0200

    "and" and "or" filters now uses Add Elastica_Filter_Abstract_Multi

--- lib/Elastica/Filter/And.php
+++ lib/Elastica/Filter/And.php
@@ -2,22 +2,11 @@
 /**
  * And Filter
  *
- * @uses Elastica_Filter_Abstract
+ * @uses Elastica_Filter_Abstract_Multi
  * @category Xodoa
  * @package Elastica
  * @author Lee Parker, Nicolas Ruflin <spam@ruflin.com>
  * @link http://www.elasticsearch.org/guide/reference/query-dsl/and-filter.html
  */
-class Elastica_Filter_And extends Elastica_Filter_Abstract
-{
-	/**
-	 * Adds one more filter to the and filter
-	 *
-	 * @param Elastica_Filter_Abstract $filter
-	 * @return Elastica_Filter_And Current object
-	 */
-	public function addFilter(Elastica_Filter_Abstract $filter) {
-		$this->_params[] = $filter->toArray();
-		return $this;
-	}
+class Elastica_Filter_And extends Elastica_Filter_Abstract_Multi {
 }
--- lib/Elastica/Filter/Or.php
+++ lib/Elastica/Filter/Or.php
@@ -2,39 +2,11 @@
 /**
  * Or Filter
  *
- * @uses Elastica_Filter_Abstract
+ * @uses Elastica_Filter_Abstract_Multi
  * @category Xodoa
  * @package Elastica
  * @author Nicolas Ruflin <spam@ruflin.com>
  * @link http://www.elasticsearch.com/docs/elasticsearch/rest_api/query_dsl/or_filter/
  */
-class Elastica_Filter_Or extends Elastica_Filter_Abstract {
-	/**
-	 * Filter array
-	 * 
-	 * @var array Filter
-	 */
-	protected $_filters = array();
-
-	/**
-	 * Adds filter to or filter
-	 *
-	 * @param Elastica_Filter_Abstract $filter Filter object
-	 * @return Elastica_Filter_Or Filter object
-	 */
-	public function addFilter(Elastica_Filter_Abstract $filter) {
-		$this->_filters[] = $filter->toArray();
-		return $this;
-	}
-
-	/**
-	 * Convers current object to array.
-	 *
-	 * @see Elastica_Filter_Abstract::toArray()
-	 * @return array Or array
-	 */
-	public function toArray() {
-		$this->setParams($this->_filters);
-		return parent::toArray();
-	}
+class Elastica_Filter_Or extends Elastica_Filter_Abstract_Multi {
 }
