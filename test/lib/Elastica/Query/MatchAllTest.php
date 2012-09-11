3629b756798ab3e3f03eee863a5fdacbd84f6c7a
Nicolas Ruflin <spam@ruflin.com>
2012-09-11 07:31:12 +0200

    test case for match all query with multiple indices

--- test/lib/Elastica/Query/MatchAllTest.php
+++ test/lib/Elastica/Query/MatchAllTest.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/../../../bootstrap.php';
 
-class Elastica_Query_MatchAllTest extends PHPUnit_Framework_TestCase
+class Elastica_Query_MatchAllTest extends Elastica_Test
 {
     public function testToArray()
     {
@@ -11,4 +11,21 @@ class Elastica_Query_MatchAllTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($expectedArray, $query->toArray());
     }
+
+	public function testMatchAllIndicesTypes() {
+		$index1 = $this->_createIndex('test1');
+		$index2 = $this->_createIndex('test1');
+
+		$doc = new Elastica_Document(1, array('name' => 'ruflin'));
+		$index1->getType('test')->addDocument($doc);
+		$index2->getType('test')->addDocument($doc);
+
+		$index1->refresh();
+		$index2->refresh();
+
+		$search = new Elastica_Search($index1->getClient());
+		$resultSet = $search->search(new Elastica_Query_MatchAll());
+
+		$this->assertEquals(2, $resultSet->count());
+	}
 }
