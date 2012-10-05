05487b4606c6a6cd7e9de4a614a115e3d18d1497
Ray Ward <ray.ward@bigcommerce.com>
2012-10-05 15:29:32 +1000

    Added getStatus test.

--- test/lib/Elastica/Cluster/HealthTest.php
+++ test/lib/Elastica/Cluster/HealthTest.php
@@ -3,9 +3,23 @@ require_once dirname(__FILE__) . '/../../../bootstrap.php';
 
 class Elastica_Cluster_HealthTest extends Elastica_Test
 {
+    /**
+     * @var Elastica_Cluster_Health
+     */
+    protected $_health;
+
+    public function setUp()
+    {
+        $this->_health = new Elastica_Cluster_Health($this->_getClient());
+    }
+
     public function testGetClusterName()
     {
-        $health = new Elastica_Cluster_Health($this->_getClient());
-        $this->assertEquals('elasticsearch', $health->getClusterName());
+        $this->assertEquals('elasticsearch', $this->_health->getClusterName());
+    }
+
+    public function testGetStatus()
+    {
+        $this->_assertEquals('green', $this->_health->getStatus());
     }
 }
