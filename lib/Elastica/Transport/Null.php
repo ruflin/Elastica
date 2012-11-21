2e718c3b6f100ba386855b53dca465ddf78f6e8b
James Boehmer <james.boehmer@gmail.com>
2012-11-20 19:04:50 -0500

    Added a more complete response object with fields set to 0. Added a more complete unit test to validate field existence and values.

--- lib/Elastica/Transport/Null.php
+++ lib/Elastica/Transport/Null.php
@@ -15,6 +15,21 @@ class Elastica_Transport_Null extends Elastica_Transport_Abstract
      */
     public function exec(array $params)
     {
-     	return new Elastica_Response(null);
+    	$response = array(
+    			"took" => 0,
+    			"timed_out" => FALSE,
+    			"_shards" => array(
+    					"total" => 0,
+    					"successful" => 0,
+    					"failed" => 0
+    					),
+    			"hits" => array(
+    					"total" => 0,
+    					"max_score" => NULL,
+    					"hits" => array()
+    					),
+    			"params" => $params
+    			);
+     	return new Elastica_Response(json_encode($response));
     }
 }
