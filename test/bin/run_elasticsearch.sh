609b08c0a3131330d5680ed98e6c9dca8c6f8fca
Nicolas Ruflin <spam@ruflin.com>
2012-12-16 10:44:37 +0100

    Update to elasticsearch 0.20.1 and memcache 2.2.7

--- test/bin/run_elasticsearch.sh
+++ test/bin/run_elasticsearch.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-wget http://cloud.github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.11.tar.gz
-tar -xzf elasticsearch-0.19.11.tar.gz
-sed 's/# index.number_of_shards: 1/index.number_of_shards: 2/' elasticsearch-0.19.11/config/elasticsearch.yml > elasticsearch-0.19.11/config/elasticsearch.yml
-sed 's/# index.number_of_replicas: 0/index.number_of_replicas: 0/' elasticsearch-0.19.11/config/elasticsearch.yml > elasticsearch-0.19.11/config/elasticsearch.yml
-sed 's/# discovery.zen.ping.multicast.enabled: false/discovery.zen.ping.multicast.enabled: false/' elasticsearch-0.19.11/config/elasticsearch.yml > elasticsearch-0.19.11/config/elasticsearch.yml
-elasticsearch-0.19.11/bin/plugin -install elasticsearch/elasticsearch-mapper-attachments/1.6.0
+wget http://cloud.github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.20.1.tar.gz
+tar -xzf elasticsearch-0.20.1.tar.gz
+sed 's/# index.number_of_shards: 1/index.number_of_shards: 2/' elasticsearch-0.20.1/config/elasticsearch.yml > elasticsearch-0.20.1/config/elasticsearch.yml
+sed 's/# index.number_of_replicas: 0/index.number_of_replicas: 0/' elasticsearch-0.20.1/config/elasticsearch.yml > elasticsearch-0.20.1/config/elasticsearch.yml
+sed 's/# discovery.zen.ping.multicast.enabled: false/discovery.zen.ping.multicast.enabled: false/' elasticsearch-0.20.1/config/elasticsearch.yml > elasticsearch-0.20.1/config/elasticsearch.yml
+elasticsearch-0.20.1/bin/plugin -install elasticsearch/elasticsearch-mapper-attachments/1.6.0
 
 export JAVA_OPTS="-server"
-elasticsearch-0.19.11/bin/elasticsearch &
+elasticsearch-0.20.1/bin/elasticsearch &
+elasticsearch-0.20.1/bin/elasticsearch &
 
-echo "Waiting until elasticsearch is ready on port 9200"
+echo "Waiting until elasticsearch node 1 is ready on port 9200"
 while [[ -z `curl -s 'http://localhost:9200' ` ]]
 do 
 	echo -n "."
 	sleep 2s
 done
 
-echo "elasticsearch is up"
+echo "Waiting until elasticsearch node 2 is ready on port 9201"
+while [[ -z `curl -s 'http://localhost:9201' ` ]]
+do 
+	echo -n "."
+	sleep 2s
+done
+
+echo "two elasticsearch nodes are up"
