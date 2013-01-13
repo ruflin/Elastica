178b481df99fba438082862e750878f520338744
Sébastien Lavoie <github@lavoie.sl>
2013-01-13 16:14:47 -0500

    changed test/bootstrap to use composer/autoload
    added composer in .travis.yml

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,20 +1,5 @@
 <?php
 
-defined('BASE_PATH') || define('BASE_PATH', realpath(dirname(__FILE__)));
+defined('BASE_PATH') || define('BASE_PATH', realpath(__DIR__));
 
-// adds elasticsearch to the include path
-set_include_path(
-    get_include_path() . PATH_SEPARATOR .
-    BASE_PATH . '/../lib' . PATH_SEPARATOR .
-    BASE_PATH . '/lib'
-);
-
-function elasticaAutoload($class)
-{
-    if (substr($class, 0, 9) == 'Elastica_') {
-        $file = str_replace('_', '/', $class) . '.php';
-        require_once $file;
-    }
-}
-
-spl_autoload_register('elasticaAutoload');
+require dirname(__DIR__) . '/vendor/autoload.php';
