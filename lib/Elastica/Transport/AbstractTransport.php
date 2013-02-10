b4a4d06b40063977a7048ceac9ab8a17ca73f785
Christer Edvartsen <cogo@starzinger.net>
2013-02-10 21:16:05 +0100

    Removed _path, _method and _data as these are not used by any transport, and added a create method that can be used as a factory for transports. Also added a test case for the factory.

--- lib/Elastica/Transport/AbstractTransport.php
+++ lib/Elastica/Transport/AbstractTransport.php
@@ -4,6 +4,8 @@ namespace Elastica\Transport;
 
 use Elastica\Connection;
 use Elastica\Request;
+use Elastica\Exception\InvalidException;
+use Elastica\Param;
 
 /**
  * Elastica Abstract Transport object
@@ -12,30 +14,8 @@ use Elastica\Request;
  * @package Elastica
  * @author Nicolas Ruflin <spam@ruflin.com>
  */
-abstract class AbstractTransport
+abstract class AbstractTransport extends Param
 {
-    /**
-     * Path
-     *
-     * @var string path
-     */
-    protected $_path;
-
-    /**
-     * Method
-     *
-     * @var string method
-     * @todo set default method?
-     */
-    protected $_method;
-
-    /**
-     * Data
-     *
-     * @var array Data
-     */
-    protected $_data;
-
     /**
      * @var \Elastica\Connection
      */
@@ -46,9 +26,11 @@ abstract class AbstractTransport
      *
      * @param \Elastica\Connection $connection Connection object
      */
-    public function __construct(Connection $connection)
+    public function __construct(Connection $connection = null)
     {
-        $this->_connection = $connection;
+        if ($connection) {
+            $this->setConnection($connection);
+        }
     }
 
     /**
@@ -60,6 +42,14 @@ abstract class AbstractTransport
     }
 
     /**
+     * @param \Elastica\Connection $connection Connection object
+     */
+    public function setConnection(Connection $connection)
+    {
+        $this->_connection = $connection;
+    }
+
+    /**
      * Executes the transport request
      *
      * @param  \Elastica\Request  $request Request object
@@ -67,4 +57,53 @@ abstract class AbstractTransport
      * @return \Elastica\Response Response object
      */
     abstract public function exec(Request $request, array $params);
+
+    /**
+     * Create a transport
+     *
+     * The $transport parameter can be one of the following values:
+     *
+     * * string: The short name of a transport. For instance "Http", "Memcache" or "Thrift"
+     * * object: An already instantiated instance of a transport
+     * * array: An array with a "type" key which must be set to one of the two options. All other
+     *          keys in the array will be set as parameters in the transport instance
+     *
+     * @param mixed $transport A transport definition
+     * @param \Elastica\Connection $connection A connection instance
+     * @param array $params Parameters for the transport class
+     * @throws \Elastica\Exception\InvalidException
+     * @return AbstractTransport
+     */
+    public static function create($transport, Connection $connection, array $params = array())
+    {
+        if (is_array($transport) && isset($transport['type'])) {
+            $transportParams = $transport;
+            unset($transportParams['type']);
+
+            $params = array_replace($params, $transportParams);
+            $transport = $transport['type'];
+        }
+
+        if (is_string($transport)) {
+            $className = 'Elastica\\Transport\\' . $transport;
+
+            if (!class_exists($className)) {
+                throw new InvalidException('Invalid transport');
+            }
+
+            $transport = new $className;
+        }
+
+        if ($transport instanceof AbstractTransport) {
+            $transport->setConnection($connection);
+
+            foreach ($params as $key => $value) {
+                $transport->setParam($key, $value);
+            }
+        } else {
+            throw new InvalidException('Invalid transport');
+        }
+
+        return $transport;
+    }
 }
