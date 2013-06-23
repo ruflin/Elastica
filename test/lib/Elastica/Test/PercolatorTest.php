b98704b0ddfd033432f2483dc45313ecf6409d2d
Jasper van Wanrooy <jasper@vanwanrooy.net>
2013-06-23 21:29:59 +0200

    Refactored basic test to match documents to become better readable.

--- test/lib/Elastica/Test/PercolatorTest.php
+++ test/lib/Elastica/Test/PercolatorTest.php
@@ -36,32 +36,16 @@ class PercolatorTest extends BaseTest
 
     public function testMatchDoc()
     {
-        $client = new Client(array('persistent' => false));
-        $index = $client->getIndex('elastica_test');
-        $index->create(array('index' => array('number_of_shards' => 1, 'number_of_replicas' => 0)), true);
-
+        $index = $this->_createIndex();
         $percolator = new Percolator($index);
 
         $percolatorName = 'percotest';
-        $percolatorSecondName = 'percotest_color';
 
         $query = new Term(array('name' => 'ruflin'));
         $response = $percolator->registerQuery($percolatorName, $query);
 
         $this->assertTrue($response->isOk());
         $this->assertFalse($response->hasError());
-
-        // Add index with the same query and additional parameter
-        $secondParamKey = 'color';
-        $secondParamValue = 'blue';
-        $querySecond = new Query();
-        $querySecond->setQuery($query);
-        $querySecond->setParam($secondParamKey, $secondParamValue);
-        $responseSecond = $percolator->registerQuery($percolatorSecondName, $querySecond);
-
-        // Check if it's ok
-        $this->assertTrue($responseSecond->isOk());
-        $this->assertFalse($responseSecond->hasError());
 
         $doc1 = new Document();
         $doc1->set('name', 'ruflin');
@@ -76,28 +60,10 @@ class PercolatorTest extends BaseTest
         $matches1 = $percolator->matchDoc($doc1);
 
         $this->assertTrue(in_array($percolatorName, $matches1));
-        $this->assertTrue(in_array($percolatorSecondName, $matches1));
-        $this->assertEquals(2, count($matches1));
+        $this->assertCount(1, $matches1);
 
         $matches2 = $percolator->matchDoc($doc2);
         $this->assertEmpty($matches2);
-
-        // Test using document with additional parameter
-        $docSecond = $doc1;
-        $docSecond->set($secondParamKey, $secondParamValue);
-
-        // Create term for our parameter to filter data while percolating
-        $secondTerm = new Term(array($secondParamKey => $secondParamValue));
-        $secondQuery = new Query();
-        $secondQuery->setQuery($secondTerm);
-
-        // Match the document to percolator queries and filter results using optional parameter
-        $secondMatches = $percolator->matchDoc($docSecond, $secondQuery);
-
-        // Check if only one proper index was returned
-        $this->assertFalse(in_array($percolatorName, $secondMatches));
-        $this->assertTrue(in_array($percolatorSecondName, $secondMatches));
-        $this->assertEquals(1, count($secondMatches));
     }
 
     /**
@@ -117,8 +83,8 @@ class PercolatorTest extends BaseTest
         $this->assertFalse($response->hasError());
 
         // refreshing is required in order to ensure the query is really ready for execution.
-        $index = new Index($index->getClient(), '_percolator');
-        $index->refresh();
+        $percolatorIndex = new Index($index->getClient(), '_percolator');
+        $percolatorIndex->refresh();
         
         // step two: match a document which should match the kuku query when filtered on the blue color
         $doc = new Document();
