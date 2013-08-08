7fdb55857a2700c2ddee5d04ae0f1f7a7a748ef4
mjhu <mjhu91@gmail.com>
2013-08-08 08:14:40 -0700

    added rescore query

---
+++ lib/Elastica/Query/Rescore.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Elastica\Query;
+
+/**
+ * Rescore query
+ *
+ * @category Xodoa
+ * @package Elastica
+ * @author Jason Hu <mjhu91@gmail.com>
+ * @link http://www.elasticsearch.org/guide/reference/api/search/rescore/
+ */
+class Rescore extends AbstractQuery
+{
+    /**
+     * Constructor
+     *
+     * @param string|\Elastica\Query\AbstractQuery $rescore_query
+     * @param string|\Elastica\Query\AbstractQuery $query
+     */
+    public function __construct($query = null, $rescore_query= null)
+    {
+        $this->setQuery($query);
+        $this->setParam('rescore', array());
+        $this->setRescoreQuery($rescore_query);
+    }
+
+    /**
+     * Sets query object
+     *
+     * @param  string|\Elastica\Query|\Elastica\Query\AbstractQuery $query
+     * @return \Elastica\Query\Rescore
+     */
+    public function setQuery($query)
+    {
+        $query = BaseQuery::create($query);
+        $data = $query->toArray();
+
+        return $this->setParam('query', $data['query']);
+    }
+
+    /**
+     * Sets rescore_query object
+     *
+     * @param  string|\Elastica\Query|\Elastica\Query\AbstractQuery $query
+     * @return \Elastica\Query\Rescore
+     */
+    public function setRescoreQuery($rescore_query)
+    {
+        $query = BaseQuery::create($rescore_query);
+        $data = $query->toArray();
+
+        $rescore = $this->getParam('rescore');
+        $rescore['query']['rescore_query'] = $data['query'];
+
+        return $this->setParam('rescore', $rescore);
+    }
+
+    /**
+     * Sets window_size
+     *
+     * @param int $size
+     * @return \Elastica\Query\Rescore
+     */
+    public function setWindowSize($size)
+    {
+        $rescore = $this->getParam('rescore');
+        $rescore['query']['window_size'] = $size;
+
+        return $this->setParam('rescore', $rescore);
+    }
+}
