c143f1a9448383f751f7f3a990460b1404813ff0
mjhu <mjhu91@gmail.com>
2013-08-08 08:22:15 -0700

    added rescore test stubs

---
+++ test/lib/Elastica/Test/Query/RescoreTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Elastica\Test\Query;
+
+use Elastica\Query\Rescore;
+use Elastica\Test\Base as BaseTest;
+
+class RescoreTest extends BaseTest
+{
+    public function dataProviderSampleQueries()
+    {
+        return array(
+        );
+    }
+    /**
+     * @dataProvider dataProviderSampleQueries
+     */
+    public function testSimple($filter, $expected)
+    {
+    }
+
+    public function testToArray()
+    {
+    }
+
+    public function testConstruct()
+    {
+    }
+
+    public function testConstructEmpty()
+    {
+    }
+}
