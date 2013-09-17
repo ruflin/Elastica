893d20af652be0feb04f3301ae6db6e829c510f8
Peter WONG <luiges90@gmail.com>
2013-09-17 19:45:01 +0800

    Add some Match query parameters for MultiMatch query

--- lib/Elastica/Query/MultiMatch.php
+++ lib/Elastica/Query/MultiMatch.php
@@ -8,6 +8,7 @@ namespace Elastica\Query;
  * @category Xodoa
  * @package Elastica
  * @author Rodolfo Adhenawer Campagnoli Moraes <adhenawer@gmail.com>
+ * @author Peter WONG Wing Lun <luiges90@gmail.com>
  * @link http://www.elasticsearch.org/guide/reference/query-dsl/multi-match-query.html
  */
 class MultiMatch extends AbstractQuery
@@ -59,5 +60,53 @@ class MultiMatch extends AbstractQuery
     public function setTieBreaker($tieBreaker = 0.0)
     {
         return $this->setParam('tie_breaker', $tieBreaker);
+    }
+    
+    /**
+     * Sets operator for Match Query
+     *
+     * If not set, defaults to 'or'
+     *
+     * @param  string                             $operator
+     * @return \Elastica\Query\MultiMatch Current object
+     */
+    public function setOperator($operator = 'or')
+    {
+        return $this->setParam('operator', $operator);
+    }
+    
+    /**
+     * Set field minimum should match for Match Query
+     *
+     * @param  int                       $minimumShouldMatch
+     * @return \Elastica\Query\Match
+     */
+    public function setMinimumShouldMatch($minimumShouldMatch)
+    {
+        return $this->setParam('minimum_should_match', (int) $minimumShouldMatch);
+    }
+    
+    /**
+     * Set zero terms query for Match Query
+     *
+     * If not set, default to 'none'
+     * 
+     * @param  string                       $zeroTermQuery
+     * @return \Elastica\Query\Match
+     */
+    public function setZeroTermsQuery($zeroTermQuery = 'none')
+    {
+        return $this->setParam('zero_terms_query', $zeroTermQuery);
+    }
+    
+    /**
+     * Set cutoff frequency for Match Query
+     * 
+     * @param  float                       $cutoffFrequency
+     * @return \Elastica\Query\Match
+     */
+    public function setCutoffFrequency($cutoffFrequency)
+    {
+        return $this->setParam('cutoff_frequency', $cutoffFrequency);
     }
 }
