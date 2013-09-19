b32d16b0afb3af3641bd5f3bf1adee04505b2d00
Imanol Cea <imanol@simbiotica.es>
2013-09-19 16:24:58 +0200

    Improve term suggest tests

--- test/lib/Elastica/Test/Suggest/Term.php
+++ test/lib/Elastica/Test/Suggest/Term.php
@@ -53,4 +53,67 @@ class TermTest extends BaseTest
 
         $this->assertEquals($expectedArray, $query->toArray());
     }
+
+    public function testSuggestResults()
+    {
+        $client = $this->_getClient();
+        $search = new Search($client);
+
+        $index = $client->getIndex('zero');
+        $index->create(array('index' => array('number_of_shards' => 1, 'number_of_replicas' => 0)), true);
+
+        $docs = array();
+        $docs[] = new Document(5, array('id' => 1, 'text' => 'GitHub'));
+        $docs[] = new Document(6, array('id' => 1, 'text' => 'Elastic'));
+        $docs[] = new Document(7, array('id' => 1, 'text' => 'Search'));
+        $docs[] = new Document(3, array('id' => 1, 'text' => 'Food'));
+        $docs[] = new Document(4, array('id' => 1, 'text' => 'Folks'));
+        $type = $index->getType('zeroType');
+        $type->addDocuments($docs);
+        $index->refresh();
+
+        $search->addIndex($index)->addType($type);
+
+        $suggest = new Term();
+        $suggest->addTerm('suggest1', array('text' => 'Foor', 'term' => array('field' => '_all', 'size' => 4)));
+        $suggest->addTerm('suggest2', array('text' => 'Girhub', 'term' => array('field' => '_all', 'size' => 4)));
+
+        $search->addSuggest($suggest);
+        $result = $search->search();
+
+        $this->assertEquals(2, $result->countSuggests());
+        
+        $suggests = $result->getSuggests();
+
+        $this->assertEquals('Github', $suggests['suggest2']['options'][0]['text']);
+        $this->assertEquals('Food', $suggests['suggest1']['options'][0]['text']);
+    }
+
+    public function testSuggestNoResults()
+    {
+        $client = $this->_getClient();
+        $search = new Search($client);
+
+        $index = $client->getIndex('zero');
+        $index->create(array('index' => array('number_of_shards' => 1, 'number_of_replicas' => 0)), true);
+
+        $docs = array();
+        $docs[] = new Document(5, array('id' => 1, 'text' => 'GitHub'));
+        $docs[] = new Document(6, array('id' => 1, 'text' => 'Elastic'));
+        $docs[] = new Document(3, array('id' => 1, 'text' => 'Food'));
+        $docs[] = new Document(4, array('id' => 1, 'text' => 'Folks'));
+        $type = $index->getType('zeroType');
+        $type->addDocuments($docs);
+        $index->refresh();
+
+        $search->addIndex($index)->addType($type);
+
+        $suggest = new Term();
+        $suggest->addTerm('suggest1', array('text' => 'Search', 'term' => array('field' => '_all', 'size' => 4)));
+
+        $search->addSuggest($suggest);
+        $result = $search->search();
+
+        $this->assertEquals(0, $result->countSuggests());
+    }
 }
