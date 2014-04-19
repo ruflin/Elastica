08c79fe5eaca39133449c1d4b41dd0ebdc190670
ruflin <spam@ruflin.com>
2014-04-19 14:17:14 +0200

    Fixed GeoPolygonTest for elasticsearch 1.1.0

--- test/lib/Elastica/Test/Filter/GeoPolygonTest.php
+++ test/lib/Elastica/Test/Filter/GeoPolygonTest.php
@@ -19,7 +19,7 @@ class GeoPolygonTest extends BaseTest
         $type = $index->getType('test');
 
         // Set mapping
-        $type->setMapping(array('point' => array('type' => 'geo_point')));
+        $type->setMapping(array('location' => array('type' => 'geo_point')));
 
         // Add doc 1
         $doc1 = new Document(1,
@@ -28,7 +28,7 @@ class GeoPolygonTest extends BaseTest
             )
         );
 
-        $doc1->addGeoPoint('point', 17, 19);
+        $doc1->addGeoPoint('location', 17, 19);
         $type->addDocument($doc1);
 
         // Add doc 2
@@ -38,7 +38,7 @@ class GeoPolygonTest extends BaseTest
             )
         );
 
-        $doc2->addGeoPoint('point', 30, 40);
+        $doc2->addGeoPoint('location', 30, 40);
         $type->addDocument($doc2);
 
         $index->refresh();
@@ -46,7 +46,7 @@ class GeoPolygonTest extends BaseTest
         // Only one point should be in polygon
         $query = new Query();
         $points = array(array(16, 16), array(16, 20), array(20, 20), array(20, 16), array(16, 16));
-        $geoFilter = new GeoPolygon('point', compact('points'));
+        $geoFilter = new GeoPolygon('location', $points);
 
         $query = new Query(new MatchAll());
         $query->setFilter($geoFilter);
@@ -55,7 +55,7 @@ class GeoPolygonTest extends BaseTest
         // Both points should be inside
         $query = new Query();
         $points = array(array(16, 16), array(16, 40), array(40, 40), array(40, 16), array(16, 16));
-        $geoFilter = new GeoPolygon('point', compact('points'));
+        $geoFilter = new GeoPolygon('location', $points);
 
         $query = new Query(new MatchAll());
         $query->setFilter($geoFilter);
