c66176dffe6315a2a23a9d53ac0670761663c90e
sten <sten.govaerts@epfl.ch>
2014-04-22 11:36:34 +0200

    fixes toArray function of Elastica\Filter\Bool that did not allow children Bool filters, closes #594

---
+++ test/lib/Elastica/Test/Filter/BoolTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Elastica\Test\Filter;
+
+use Elastica\Filter\Bool;
+use Elastica\Filter\Ids;
+use Elastica\Test\Base as BaseTest;
+
+class BoolTest extends BaseTest
+{
+    public function testToArray()
+    {
+        $mainBool = new Bool();
+
+        $idsFilter1 = new Ids();
+        $idsFilter1->setIds(1);
+        $idsFilter2 = new Ids();
+        $idsFilter2->setIds(2);
+        $idsFilter3 = new Ids();
+        $idsFilter3->setIds(3);
+
+        $childBool = new Bool();
+        $childBool->addShould(array($idsFilter1, $idsFilter2));
+        $mainBool->addShould(array($childBool, $idsFilter3));
+
+        $expectedArray = array(
+            'bool' => array(
+                'should' => array(
+                    array(
+                        array(
+                            'bool' => array(
+                                'should' => array(
+                                    array(
+                                        $idsFilter1->toArray(),
+                                        $idsFilter2->toArray()
+                                    )
+                                )
+                            )
+                        ),
+                        $idsFilter3->toArray()
+                    )
+                )
+            )
+        );
+
+        print_r($mainBool->toArray());
+
+        $this->assertEquals($expectedArray, $mainBool->toArray());
+    }
+}
