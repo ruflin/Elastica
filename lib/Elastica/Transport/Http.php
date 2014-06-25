6ad126a5d600418f5c1f5ab9341d96d0d27da421
Tobias Schultze <webmaster@tubo-world.de>
2014-06-25 14:40:57 +0200

    Fix '0' as valid request data
    `Index->analyze('0', array('analyzer' => 'language_query_analyzer'))` would throw an `ElasticsearchIllegalArgumentException[text is missing]` because the request data is not passed.

--- lib/Elastica/Transport/Http.php
+++ lib/Elastica/Transport/Http.php
@@ -93,7 +93,7 @@ class Http extends AbstractTransport
         $data = $request->getData();
         $httpMethod = $request->getMethod();
 
-        if (isset($data) && !empty($data)) {
+        if ('' != $data) {
             if ($this->hasParam('postWithRequestBody') && $this->getParam('postWithRequestBody') == true) {
                 $httpMethod = Request::POST;
             }
