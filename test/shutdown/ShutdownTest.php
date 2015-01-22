26628ca00070b7c7c87b05822a242a59e16e1f90
im-denisenko <im.denisenko@yahoo.com>
2015-01-22 20:16:01 +0300

    Split phpunit config to travis and virtualbox versions.

---
+++ test/shutdown/ShutdownTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use Elastica\Test\Base as BaseTest;
+
+/**
+ * These tests shuts down node/cluster, so can't be executed with rest testsuite
+ * Please use `sudo service elasticsearch restart` after every run of these tests
+ */
+class ShutdownTest extends BaseTest
+{
+    /**
+     * @test
+     */
+    public function nodeShutdown()
+    {
+        // Get cluster nodes
+        $client = $this->_getClient();
+        $cluster = $client->getCluster();
+        $nodes = $cluster->getNodes();
+
+        if (count($nodes) < 3) {
+            $this->markTestIncomplete('At least three nodes have to be running, because 1 node is shutdown');
+        }
+
+        // sayonara, wolverine, we'd never love you
+        foreach ($nodes as $node) {
+            if ($node->getName() === 'Wolverine') {
+                $node->shutdown('2s');
+                break;
+            }
+        }
+
+        // Wait until node is shutdown
+        sleep(5);
+
+        // Get nodes again
+        $client = $this->_getClient();
+        $cluster = $client->getCluster();
+        $nodes = $cluster->getNodes();
+
+        // Only two left
+        $this->assertCount(2, $nodes);
+    }
+
+    /**
+     * @test
+     * @depends nodeShutdown
+     * @expectedException \Elastica\Exception\ConnectionException
+     */
+    public function clusterShutdown()
+    {
+        // Get cluster nodes
+        $client = $this->_getClient();
+        $cluster = $client->getCluster();
+        $nodes = $cluster->getNodes();
+
+        if (count($nodes) < 2) {
+            $this->markTestIncomplete('At least two nodes have to be running, because we shuts down entire cluster');
+        }
+
+        $cluster->shutdown('2s');
+
+        sleep(5);
+
+        $client->getStatus();
+    }
+}
