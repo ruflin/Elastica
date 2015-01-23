d5c2ee3da8d74f68cdd0a69515a5cfea4036a98c
Aurelien Le Grand <alegrand@cinefil.com>
2015-01-23 10:49:41 +0100

    Add Regexp query object

---
+++ test/lib/Elastica/Test/Query/RegexpTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Elastica\Test\Query;
+
+use Elastica\Query\Regexp;
+use Elastica\Test\Base as BaseTest;
+
+class setFieldParam extends BaseTest
+{
+
+    public function testToArray()
+    {
+        $field = 'name';
+        $value = 'ruf';
+        $boost = 2;
+
+        $query = new Regexp($field, $value, $boost);
+
+        $expectedArray = array(
+            'regexp' => array(
+                $field => array(
+                    'value' => $value,
+                    'boost' => $boost
+                )
+            )
+        );
+
+        $this->assertequals($expectedArray, $query->toArray());
+    }
+}
