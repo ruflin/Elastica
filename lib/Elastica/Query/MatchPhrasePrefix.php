d0d2855c3c37a7295f59b4994714038506a0bdac
Jacques Moati <jacques@moati.net>
2015-02-17 15:09:55 +0100

    Add MatchPhrase and MatchPhrasePrefix

---
+++ lib/Elastica/Query/MatchPhrasePrefix.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Elastica\Query;
+
+/**
+ * Match Phrase Prefix query
+ *
+ * @category Xodoa
+ * @package Elastica
+ * @author Jacques Moati <jacques@moati.net>
+ * @link http://www.elasticsearch.org/guide/reference/query-dsl/match-query.html
+ */
+class MatchPhrasePrefix extends Match
+{
+}
