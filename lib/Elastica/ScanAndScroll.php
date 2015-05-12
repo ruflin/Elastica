dbfe675b2a10f9287350c6e9f6a962321962b93e
manuel <andreo.garcia@googlemail.com>
2015-05-13 00:52:42 +0200

    added Scroll Iterator

--- lib/Elastica/ScanAndScroll.php
+++ lib/Elastica/ScanAndScroll.php
@@ -3,49 +3,21 @@
 namespace Elastica;
 
 /**
- * scan and scroll object
+ * Scan and Scroll Iterator
  *
- * @category Xodoa
- * @package Elastica
  * @author Manuel Andreo Garcia <andreo.garcia@gmail.com>
+ *
  * @link http://www.elastic.co/guide/en/elasticsearch/guide/current/scan-scroll.html
  */
-class ScanAndScroll implements \Iterator
+class ScanAndScroll extends Scroll
 {
-    /**
-     * time value parameter
-     *
-     * @var string
-     */
-    public $expiryTime;
-
     /**
      * @var int
      */
     public $sizePerShard;
 
     /**
-     * @var Search
-     */
-    protected $_search;
-
-    /**
-     * @var null|string
-     */
-    protected $_nextScrollId = null;
-
-    /**
-     * @var null|string
-     */
-    protected $_lastScrollId = null;
-
-    /**
-     * @var null|ResultSet
-     */
-    protected $_currentResultSet = null;
-
-    /**
-     * Constructs scroll iterator object
+     * Constructor
      *
      * @param Search $search
      * @param string $expiryTime
@@ -53,104 +25,57 @@ class ScanAndScroll implements \Iterator
      */
     public function __construct(Search $search, $expiryTime = '1m', $sizePerShard = 1000)
     {
-        $this->_search = $search;
-        $this->expiryTime = $expiryTime;
         $this->sizePerShard = $sizePerShard;
+
+        parent::__construct($search, $expiryTime);
     }
 
     /**
-     * Return the current result set
+     * Initial scan search
      *
-     * @link http://php.net/manual/en/iterator.current.php
-     * @return ResultSet
-     */
-    public function current()
-    {
-        return $this->_currentResultSet;
-    }
-
-    /**
-     * Perform next scroll search
-     *
-     * @link http://php.net/manual/en/iterator.next.php
-     * @return void
-     */
-    public function next()
-    {
-        $this->_scroll();
-    }
-
-    /**
-     * Return the scroll id of current scroll request
-     *
-     * @link http://php.net/manual/en/iterator.key.php
-     * @return string
-     */
-    public function key()
-    {
-        return $this->_lastScrollId;
-    }
-
-    /**
-     * Returns true if current result set contains one hit
-     *
-     * @link http://php.net/manual/en/iterator.valid.php
-     * @return boolean
-     */
-    public function valid()
-    {
-        return
-            $this->_nextScrollId !== null
-            && $this->_currentResultSet !== null
-            && $this->_currentResultSet->count() > 0;
-    }
-
-    /**
-     * Start the initial scan search
      * @link http://php.net/manual/en/iterator.rewind.php
-     * @throws \Elastica\Exception\InvalidException
-     *
-     * @return void
      */
     public function rewind()
     {
-        $this->_search->getQuery()->setSize($this->sizePerShard);
+        // reset state
+        $this->_nextScrollId = null;
+        $this->_options = array(null, null, null, null);
 
-        $this->_search->setOption(Search::OPTION_SEARCH_TYPE, Search::OPTION_SEARCH_TYPE_SCAN);
-        $this->_search->setOption(Search::OPTION_SCROLL, $this->expiryTime);
+        $this->_saveOptions();
 
         // initial scan request
+        $this->_search->getQuery()->setSize($this->sizePerShard);
+        $this->_search->setOption(Search::OPTION_SCROLL, $this->expiryTime);
+        $this->_search->setOption(Search::OPTION_SCROLL_ID, null);
+        $this->_search->setOption(Search::OPTION_SEARCH_TYPE, Search::OPTION_SEARCH_TYPE_SCAN);
         $this->_setScrollId($this->_search->search());
 
-        // trigger first scroll request
-        $this->_scroll();
+        $this->_revertOptions();
+
+        // first scroll request
+        $this->next();
     }
 
     /**
-     * Perform next scroll search
-     * @throws \Elastica\Exception\InvalidException
+     * Save all search options manipulated by Scroll
      */
-    protected function _scroll()
+    protected function _saveOptions()
     {
-        $this->_search->setOption(Search::OPTION_SEARCH_TYPE, Search::OPTION_SEARCH_TYPE_SCROLL);
-        $this->_search->setOption(Search::OPTION_SCROLL_ID, $this->_nextScrollId);
-
-        $resultSet = $this->_search->search();
-        $this->_currentResultSet = $resultSet;
-        $this->_setScrollId($resultSet);
-    }
-
-    /**
-     * Save last scroll id and extract the new one if possible
-     * @param ResultSet $resultSet
-     */
-    protected function _setScrollId(ResultSet $resultSet)
-    {
-        $this->_lastScrollId = $this->_nextScrollId;
-
-        $this->_nextScrollId = null;
-        if ($resultSet->getResponse()->isOk()) {
-            $this->_nextScrollId = $resultSet->getResponse()->getScrollId();
+        $query = $this->_search->getQuery();
+        if ($query->hasParam('size')) {
+            $this->_options[3] = $query->getParam('size');
         }
+
+        parent::_saveOptions();
+    }
+
+    /**
+     * Revert search options to previously saved state
+     */
+    protected function _revertOptions()
+    {
+        $this->_search->getQuery()->setSize($this->_options[3]);
+
+        parent::_revertOptions();
     }
 }
