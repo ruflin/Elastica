ffe24b19426c07b1a5d788ea804d3c0a25fd503e
Ruflin <spam@ruflin.com>
2015-05-16 12:19:07 +0200

    Fix copy paste error in MemcacheTest

--- test/lib/Elastica/Test/Transport/MemcacheTest.php
+++ test/lib/Elastica/Test/Transport/MemcacheTest.php
@@ -149,7 +149,7 @@ class MemcacheTest extends BaseTest
      */
     public function testRequestWithLongPath()
     {
-        $index = $this->_getMemcacheClient();
+        $index = $this->_createIndex();
         $this->_waitForAllocation($index);
 
         $queryString = new QueryString(str_repeat('z', 300));
