a02b9a7e8b1e295cda5e05c2f902c6ff5feafd83
Ruflin <spam@ruflin.com>
2015-05-21 00:11:44 +0200

    Enable guzzle exception test

--- test/lib/Elastica/Test/Exception/Connection/GuzzleExceptionTest.php
+++ test/lib/Elastica/Test/Exception/Connection/GuzzleExceptionTest.php
@@ -7,8 +7,6 @@ class GuzzleExceptionTest extends AbstractExceptionTest
 {
     public static function setUpBeforeClass()
     {
-        self::markTestSkipped('pecl/memcache must be installed to run this test case');
-
         if (!class_exists('GuzzleHttp\\Client')) {
             self::markTestSkipped('guzzlehttp/guzzle package should be installed to run guzzle transport tests');
         }
