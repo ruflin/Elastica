dc6669b9ec27cabda95dc602e067f56be979e47e
Ruflin <spam@ruflin.com>
2015-05-21 10:05:51 +0200

    Reenabled thrift and memcache test

--- test/lib/Elastica/Test/Transport/ThriftTest.php
+++ test/lib/Elastica/Test/Transport/ThriftTest.php
@@ -12,8 +12,6 @@ class ThriftTest extends BaseTest
 {
     public static function setUpBeforeClass()
     {
-        self::markTestSkipped('pecl/memcache must be installed to run this test case');
-
         if (!class_exists('Elasticsearch\\RestClient')) {
             self::markTestSkipped('munkie/elasticsearch-thrift-php package should be installed to run thrift transport tests');
         }
