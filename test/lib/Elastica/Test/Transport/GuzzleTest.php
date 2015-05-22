ed4ee766bace9d40f1f55472a42a6b0e1c34c70e
Ruflin <spam@ruflin.com>
2015-05-22 13:27:40 +0200

    Skip guzzle at the beginning

--- test/lib/Elastica/Test/Transport/GuzzleTest.php
+++ test/lib/Elastica/Test/Transport/GuzzleTest.php
@@ -13,6 +13,10 @@ class GuzzleTest extends BaseTest
     public static function setUpBeforeClass()
     {
 
+        if (version_compare(phpversion(), 7, '>=')) {
+            self::markTestSkipped('Http tests currently do not work with PHP 7');
+        }
+
         if (!class_exists('GuzzleHttp\\Client')) {
             self::markTestSkipped('guzzlehttp/guzzle package should be installed to run guzzle transport tests');
         }
@@ -26,9 +30,7 @@ class GuzzleTest extends BaseTest
         }
 
         self::markTestSkipped('Http tests currently do not work with PHP 7');
-        if (version_compare(phpversion(), 7, '>=')) {
-            self::markTestSkipped('Http tests currently do not work with PHP 7');
-        }
+
     }
 
     /**
