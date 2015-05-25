f447fff6e4e472f8f71ea75f43d4fe9f1e70b7aa
Ruflin <spam@ruflin.com>
2015-05-25 21:36:45 +0200

    Reenabled guzzle tests

--- test/lib/Elastica/Test/Transport/GuzzleTest.php
+++ test/lib/Elastica/Test/Transport/GuzzleTest.php
@@ -12,7 +12,6 @@ class GuzzleTest extends BaseTest
 {
     public static function setUpBeforeClass()
     {
-        self::markTestSkipped('PHP 7');
         if (!class_exists('GuzzleHttp\\Client')) {
             self::markTestSkipped('guzzlehttp/guzzle package should be installed to run guzzle transport tests');
         }
