a19e518d9fda3707af77e54e8055d22e907dc63b
Tudor Matei <tmatei85@gmail.com>
2015-06-05 16:15:35 -0700

    Removed the deprecated warning for Type::deleteByQuery

--- lib/Elastica/Type.php
+++ lib/Elastica/Type.php
@@ -474,7 +474,6 @@ class Type implements SearchableInterface
      * @return \Elastica\Response
      *
      * @link http://www.elastic.co/guide/en/elasticsearch/reference/current/docs-delete-by-query.html
-     * @deprecated Delete by Query api is deprecated as of ES 1.5, and will be removed in ES 2.0
      */
     public function deleteByQuery($query, array $options = array())
     {
