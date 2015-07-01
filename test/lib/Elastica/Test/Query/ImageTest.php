5119161bc7a5c2535e9d84ca3645f13055b80070
Ruflin <spam@ruflin.com>
2015-07-01 09:30:03 +0200

    Fix typo in image tests

--- test/lib/Elastica/Test/Query/ImageTest.php
+++ test/lib/Elastica/Test/Query/ImageTest.php
@@ -65,7 +65,7 @@ class ImageTest extends BaseTest
      */
     public function testFromReference()
     {
-		$this->markTestSkipped('Tests skipped as plugin not working properly with ES 1.6.0. See https://github.com/ruflin/Elastica/pull/881')
+		$this->markTestSkipped('Tests skipped as plugin not working properly with ES 1.6.0. See https://github.com/ruflin/Elastica/pull/881');
         $field = 'image';
 
         $client = $this->_getClient();
@@ -113,7 +113,7 @@ class ImageTest extends BaseTest
      */
     public function testFromImage()
     {
-		$this->markTestSkipped('Tests skipped as plugin not working properly with ES 1.6.0. See https://github.com/ruflin/Elastica/pull/881')
+		$this->markTestSkipped('Tests skipped as plugin not working properly with ES 1.6.0. See https://github.com/ruflin/Elastica/pull/881');
 		
         $field = 'image';
 
