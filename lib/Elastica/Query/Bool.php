6fb3b4bb8a21dddf2cfeadf9633933fe418a8cb7
vilmos <vilmos@users.noreply.github.com>
2015-09-01 08:33:57 +0200

    fix phpdoc

--- lib/Elastica/Query/Bool.php
+++ lib/Elastica/Query/Bool.php
@@ -4,7 +4,7 @@ namespace Elastica\Query;
 /**
  * Bool query.
  *
- * This class is for backward compatibility reason for all php < 7 versions. For PHP 7 and above use BoolFilter as Bool is reserved.
+ * This class is for backward compatibility reason for all php < 7 versions. For PHP 7 and above use BoolQuery as Bool is reserved.
  *
  * @author Nicolas Ruflin <spam@ruflin.com>
  *
