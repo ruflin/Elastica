91849b964d724c8a129e2d6e7a6339208393f4a5
Yvann Boucher <yboucher@prismamedia.com>
2015-09-10 11:58:16 +0700

    Enable the support of "http.compression: true" in elasticsearch in the both ways : request / response. The body of the request is now compressed, as is the response.

--- lib/Elastica/Transport/Http.php
+++ lib/Elastica/Transport/Http.php
@@ -118,7 +118,15 @@ class Http extends AbstractTransport
             // Escaping of / not necessary. Causes problems in base64 encoding of files
             $content = str_replace('\/', '/', $content);
 
-            curl_setopt($conn, CURLOPT_POSTFIELDS, $content);
+            // An "Accept-Encoding" header containing all supported encoding types is sent
+            // Curl will decode the response automatically
+            curl_setopt($conn, CURLOPT_ENCODING, '');
+
+            // Let's precise that the request is also compressed
+            curl_setopt($conn, CURLOPT_HTTPHEADER, ['Content-Encoding: gzip']);
+
+            // Let's compress the request body
+            curl_setopt($conn, CURLOPT_POSTFIELDS, gzencode($content));
         } else {
             curl_setopt($conn, CURLOPT_POSTFIELDS, '');
         }
