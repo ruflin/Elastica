0e74f8bb625c245c15a91eea47f3c0d40b82dc6b
Evgeniy Sokolov <ewgraf@gmail.com>
2015-11-23 20:28:01 +0100

    No specify version for remove deprecated code

--- lib/Elastica/Query/Builder.php
+++ lib/Elastica/Query/Builder.php
@@ -5,7 +5,7 @@ use Elastica\Exception\InvalidException;
 use Elastica\Exception\JSONParseException;
 use Elastica\JSON;
 
-trigger_error('This builder is deprecated and will be removed in Elastica 4.0. Use new Elastica\QueryBuilder instead.', E_USER_DEPRECATED);
+trigger_error('This builder is deprecated and will be removed in further Elastica releases. Use new Elastica\QueryBuilder instead.', E_USER_DEPRECATED);
 
 /**
  * Query Builder.
@@ -13,7 +13,7 @@ trigger_error('This builder is deprecated and will be removed in Elastica 4.0. U
  * @author Chris Gedrim <chris@gedr.im>
  *
  * @link http://www.elastic.co/
- * @deprecated This builder is deprecated and will be removed in Elastica 4.0. Use new Elastica\QueryBuilder instead.
+ * @deprecated This builder is deprecated and will be removed in further Elastica releases. Use new Elastica\QueryBuilder instead.
  **/
 class Builder extends AbstractQuery
 {
