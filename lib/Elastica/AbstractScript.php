c901a16e7f17e9d6552e7fb886da28572920a9a5
Morten Hauberg <mortenhauberg@Morten-MBP.local>
2016-01-08 06:59:54 +0100

    Update changelog and add deprecated message

--- lib/Elastica/AbstractScript.php
+++ lib/Elastica/AbstractScript.php
@@ -4,6 +4,8 @@ namespace Elastica;
 use Elastica\Script\AbstractScript as BaseAbstractScript;
 /**
  * Kept for BC reasons.
+ *
+ * @deprecated Use \Elastica\Script\AbstractScript instead.
  */
 abstract class AbstractScript extends BaseAbstractScript
 {
