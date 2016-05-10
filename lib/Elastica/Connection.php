1dd5e48d26be4f30194f27dfc1e6d7daa7668c7d
ReenExe <ipourri@gmail.com>
2016-05-10 19:32:14 +0300

    clear code

--- lib/Elastica/Connection.php
+++ lib/Elastica/Connection.php
@@ -331,17 +331,15 @@ class Connection extends Param
      */
     public static function create($params = [])
     {
-        $connection = null;
-
-        if ($params instanceof self) {
-            $connection = $params;
-        } elseif (is_array($params)) {
-            $connection = new self($params);
-        } else {
-            throw new InvalidException('Invalid data type');
+        if (is_array($params)) {
+            return new self($params);
         }
 
-        return $connection;
+        if ($params instanceof self) {
+            return $params;
+        }
+
+        throw new InvalidException('Invalid data type');
     }
 
     /**
