a84c510891d68f1428fab12a41cb844afc8d65be
Mark Davidson <mark@4each.co.uk>
2016-12-20 22:07:23 +0000

    Resolve deprecated use of type in has_parent

--- lib/Elastica/Query/HasParent.php
+++ lib/Elastica/Query/HasParent.php
@@ -43,7 +43,7 @@ class HasParent extends AbstractQuery
      */
     public function setType($type)
     {
-        return $this->setParam('type', $type);
+        return $this->setParam('parent_type', $type);
     }
 
     /**
