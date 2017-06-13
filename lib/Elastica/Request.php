8feb9e0804bf281cbf3ef11bf72a2d83c6d67a5d
Mark Davidson <mark@wordery.com>
2017-06-13 11:14:12 +0100

    Request DEFAULT_CONTENT_TYPE back to application/json

--- lib/Elastica/Request.php
+++ lib/Elastica/Request.php
@@ -15,7 +15,7 @@ class Request extends Param
     const PUT = 'PUT';
     const GET = 'GET';
     const DELETE = 'DELETE';
-    const DEFAULT_CONTENT_TYPE = 'text/plain';
+    const DEFAULT_CONTENT_TYPE = 'application/json';
     const NDJSON_CONTENT_TYPE = 'application/x-ndjson';
 
     /**
