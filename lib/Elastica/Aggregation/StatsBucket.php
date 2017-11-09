e452251516c5f0bbd4d1e66d59c156e3fd661c1f
gbourgeat <gary.bourgeat@gmail.com>
2017-11-09 11:17:39 +0100

    Add Stats Bucket for use => https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-pipeline-stats-bucket-aggregation.html

---
+++ lib/Elastica/Aggregation/StatsBucket.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Elastica\Aggregation;
+
+use Elastica\Exception\InvalidException;
+
+/**
+ * Class StatsBucket.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-pipeline-stats-bucket-aggregation.html
+ */
+class StatsBucket extends AbstractAggregation
+{
+    /**
+     * @param string     $name
+     * @param string|null $bucketsPath
+     */
+    public function __construct($name, $bucketsPath = null)
+    {
+        parent::__construct($name);
+
+        if ($bucketsPath !== null) {
+            $this->setBucketsPath($bucketsPath);
+        }
+    }
+
+    /**
+     * Set the buckets_path for this aggregation.
+     *
+     * @param string $bucketsPath
+     *
+     * @return $this
+     */
+    public function setBucketsPath($bucketsPath)
+    {
+        return $this->setParam('buckets_path', $bucketsPath);
+    }
+
+    /**
+     * Set the gap policy for this aggregation.
+     *
+     * @param string $gapPolicy
+     *
+     * @return $this
+     */
+    public function setGapPolicy($gapPolicy)
+    {
+        return $this->setParam('gap_policy', $gapPolicy);
+    }
+
+    /**
+     * Set the format for this aggregation.
+     *
+     * @param string $format
+     *
+     * @return $this
+     */
+    public function setFormat($format)
+    {
+        return $this->setParam('format', $format);
+    }
+
+    /**
+     * @throws InvalidException If buckets path or script is not set
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        if (!$this->hasParam('buckets_path')) {
+            throw new InvalidException('Buckets path is required');
+        }
+
+        return parent::toArray();
+    }
+}
---
+++ test/Elastica/Aggregation/StatsBucketTest.php
@@ -0,0 +1,95 @@
+<?php
+namespace Elastica\Test\Aggregation;
+
+use Elastica\Aggregation\Histogram;
+use Elastica\Aggregation\Max;
+use Elastica\Aggregation\StatsBucket;
+use Elastica\Document;
+use Elastica\Query;
+
+class StatsBucketTest extends BaseAggregationTest
+{
+    protected function _getIndexForTest()
+    {
+        $index = $this->_createIndex();
+
+        $index->getType('test')->addDocuments([
+            Document::create(['weight' => 60, 'height' => 180, 'age' => 25]),
+            Document::create(['weight' => 70, 'height' => 156, 'age' => 32]),
+            Document::create(['weight' => 50, 'height' => 155, 'age' => 45]),
+        ]);
+
+        $index->refresh();
+
+        return $index;
+    }
+
+    /**
+     * @group functional
+     */
+    public function testBucketScriptAggregation()
+    {
+        $this->_checkScriptInlineSetting();
+
+        $bucketScriptAggregation = new StatsBucket('result', 'age_groups>max_weight');
+
+        $histogramAggregation = new Histogram('age_groups', 'age', 10);
+
+        $histogramAggregation
+            ->addAggregation((new Max('max_weight'))->setField('weight'))
+            ->addAggregation($bucketScriptAggregation);
+
+        $query = Query::create([])->addAggregation($histogramAggregation);
+
+        $results = $this->_getIndexForTest()->search($query)->getAggregation('result');
+
+        $this->assertEquals(3, $results['result']['count']);
+        $this->assertEquals(50, $results['result']['min']);
+        $this->assertEquals(70, $results['result']['max']);
+        $this->assertEquals(60, $results['result']['avg']);
+        $this->assertEquals(180, $results['result']['sum']);
+    }
+
+    /**
+     * @group unit
+     */
+    public function testConstructThroughSetters()
+    {
+        $serialDiffAgg = new StatsBucket('bucket_part');
+
+        $serialDiffAgg
+            ->setBucketsPath('age_groups>max_weight')
+            ->setFormat('test_format')
+            ->setGapPolicy(10);
+
+        $expected = [
+            'stats_bucket' => [
+                'buckets_path' => 'age_groups>max_weight',
+                'format' => 'test_format',
+                'gap_policy' => 10,
+            ],
+        ];
+
+        $this->assertEquals($expected, $serialDiffAgg->toArray());
+    }
+
+    /**
+     * @group unit
+     * @expectedException \Elastica\Exception\InvalidException
+     */
+    public function testToArrayInvalidBucketsPath()
+    {
+        $serialDiffAgg = new StatsBucket('bucket_part');
+        $serialDiffAgg->toArray();
+    }
+
+    /**
+     * @group unit
+     * @expectedException \Elastica\Exception\InvalidException
+     */
+    public function testToArrayInvalidScript()
+    {
+        $serialDiffAgg = new StatsBucket('bucket_part', ['path' => 'agg']);
+        $serialDiffAgg->toArray();
+    }
+}
