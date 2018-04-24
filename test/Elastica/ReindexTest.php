bc897a5ba8e5d99cc21d25366433e9d8692ecb55
Davis Caics <davis.caics@check24.de>
2018-04-24 09:35:06 +0200

    make reindex more flexible by implementing Param

--- test/Elastica/ReindexTest.php
+++ test/Elastica/ReindexTest.php
@@ -140,6 +140,23 @@ class ReindexTest extends Base
     }
 
     /**
+     * @group functional
+     */
+    public function testReindexWithFalseSetOnWaitForCompletion()
+    {
+        $oldIndex = $this->_createIndex('idx1', true, 2);
+        $this->_addDocs($oldIndex->getType('reindexTest'), 10);
+
+        $newIndex = $this->_createIndex('idx2', true, 2);
+
+        $reindex = new Reindex($oldIndex, $newIndex);
+        $reindex->setWaitForCompletion(Reindex::WAIT_FOR_COMPLETION_FALSE);
+        $reindex->run();
+
+        $this->assertNotEmpty($reindex->getTaskId());
+    }
+
+    /**
      * @param Type $type
      * @param int  $docs
      *
