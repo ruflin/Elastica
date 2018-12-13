9af97b3a0d7206c71d1d32e71eef886fa71a5555
Malte Hübner <maltehuebner@gmx.org>
2018-12-13 09:10:35 +0100

    Implemented feedback from pull request.

--- lib/Elastica/Aggregation/BucketSelector.php
+++ lib/Elastica/Aggregation/BucketSelector.php
@@ -7,7 +7,7 @@ use Elastica\Exception\InvalidException;
  * Class BucketSelector.
  * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-pipeline-bucket-selector-aggregation.html
  */
-class BucketSelector extends AbstractAggregation
+class BucketSelector extends AbstractSimpleAggregation
 {
     /**
      * @param string      $name
@@ -40,43 +40,15 @@ class BucketSelector extends AbstractAggregation
     }
 
     /**
-     * Set the script for this aggregation.
-     *
-     * @param string $script
-     *
-     * @return $this
-     */
-    public function setScript($script)
-    {
-        return $this->setParam('script', $script);
-    }
-
-    /**
      * Set the gap policy for this aggregation.
      *
      * @param string $gapPolicy
      *
      * @return $this
      */
-    public function setGapPolicy($gapPolicy)
+    public function setGapPolicy($gapPolicy = 'skip')
     {
         return $this->setParam('gap_policy', $gapPolicy);
-    }
-
-    /**
-     * @throws InvalidException If buckets path or script is not set
-     *
-     * @return array
-     */
-    public function toArray()
-    {
-        if (!$this->hasParam('buckets_path')) {
-            throw new InvalidException('Buckets path is required');
-        } elseif (!$this->hasParam('script')) {
-            throw new InvalidException('Script parameter is required');
-        }
-
-        return parent::toArray();
     }
 }
 
