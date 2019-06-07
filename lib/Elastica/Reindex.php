f070d3bc9f9836648efd120a6164f85c6831da4b
Damien Alexandre <mobman02@gmail.com>
2019-06-07 12:56:46 +0200

    Improvements on the Reindex API to support more options (#1637)
    This PR replace #1494 and add some missing options and tests. Thanks to @daviscaics for the initial contribution.
    
    The Reindex API now supports all the options listed on the documentation https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-reindex.html - tests are added too for Script, remote, etc.
    
    ## BC Break
    
    I chose to introduce some BC Breaks and I hope that's ok.
    
    - the `run` method was also calling `refresh()` on the new Index. So it's impossible to reindex an Index and make sure it's not yet refreshed _for some reason, like doing other indexation in that index after the reindex_. It does not make sense to force a refresh on the users. If that's really an issue, I can use the REFRESH Param value with a default value to "true", to keep the current behavior, but still allow to disable it;
    - the `run` method was returning the new Index object, that's not consistent with other API and that does not allow to read the response content (is there any conflict, how long does it took...). So I changed the returned value.
    
    ## Still not supported
    
    - the ability to set multiple index as the source index

--- lib/Elastica/Reindex.php
+++ lib/Elastica/Reindex.php
@@ -3,8 +3,10 @@
 namespace Elastica;
 
 use Elastica\Query\AbstractQuery;
+use Elastica\Script\AbstractScript;
+use Elastica\Script\Script;
 
-class Reindex
+class Reindex extends Param
 {
     const VERSION_TYPE = 'version_type';
     const VERSION_TYPE_INTERNAL = 'internal';
@@ -16,6 +18,18 @@ class Reindex
     const TYPE = 'type';
     const SIZE = 'size';
     const QUERY = 'query';
+    const SORT = 'sort';
+    const SCRIPT = 'script';
+    const SOURCE = '_source';
+    const REMOTE = 'remote';
+    const SLICE = 'slice';
+    const REFRESH = 'refresh';
+    const WAIT_FOR_COMPLETION = 'wait_for_completion';
+    const WAIT_FOR_COMPLETION_FALSE = 'false';
+    const WAIT_FOR_ACTIVE_SHARDS = 'wait_for_active_shards';
+    const TIMEOUT = 'timeout';
+    const SCROLL = 'scroll';
+    const REQUESTS_PER_SECOND = 'requests_per_second';
 
     /**
      * @var Index
@@ -32,41 +46,50 @@ class Reindex
      */
     protected $_options;
 
-    public function __construct(Index $oldIndex, Index $newIndex, array $options = [])
+    /**
+     * @var Response|null
+     */
+    protected $_lastResponse;
+
+    public function __construct(Index $oldIndex, Index $newIndex, array $params = [])
     {
         $this->_oldIndex = $oldIndex;
         $this->_newIndex = $newIndex;
-        $this->_options = $options;
+
+        $this->setParams($params);
     }
 
-    public function run()
+    public function run(): Response
     {
-        $body = $this->_getBody($this->_oldIndex, $this->_newIndex, $this->_options);
+        $body = $this->_getBody($this->_oldIndex, $this->_newIndex, $this->getParams());
 
         $reindexEndpoint = new \Elasticsearch\Endpoints\Reindex();
+        $params = \array_intersect_key($this->getParams(), \array_fill_keys($reindexEndpoint->getParamWhitelist(), null));
+        $reindexEndpoint->setParams($params);
         $reindexEndpoint->setBody($body);
 
-        $this->_oldIndex->getClient()->requestEndpoint($reindexEndpoint);
-        $this->_newIndex->refresh();
+        $this->_lastResponse = $this->_oldIndex->getClient()->requestEndpoint($reindexEndpoint);
 
-        return $this->_newIndex;
+        return $this->_lastResponse;
     }
 
-    protected function _getBody($oldIndex, $newIndex, $options)
+    protected function _getBody(Index $oldIndex, Index $newIndex, array $params): array
     {
         $body = \array_merge([
-            'source' => $this->_getSourcePartBody($oldIndex, $options),
-            'dest' => $this->_getDestPartBody($newIndex, $options),
-        ], $this->_resolveBodyOptions($options));
+            'source' => $this->_getSourcePartBody($oldIndex, $params),
+            'dest' => $this->_getDestPartBody($newIndex, $params),
+        ], $this->_resolveBodyOptions($params));
+
+        $body = $this->_setBodyScript($body);
 
         return $body;
     }
 
-    protected function _getSourcePartBody(Index $index, array $options)
+    protected function _getSourcePartBody(Index $index, array $params): array
     {
         $sourceBody = \array_merge([
             'index' => $index->getName(),
-        ], $this->_resolveSourceOptions($options));
+        ], $this->_resolveSourceOptions($params));
 
         $sourceBody = $this->_setSourceQuery($sourceBody);
         $sourceBody = $this->_setSourceType($sourceBody);
@@ -74,43 +97,44 @@ class Reindex
         return $sourceBody;
     }
 
-    protected function _getDestPartBody(Index $index, array $options)
+    protected function _getDestPartBody(Index $index, array $params): array
     {
-        return \array_merge([
+        $destBody = \array_merge([
             'index' => $index->getName(),
-        ], $this->_resolveDestOptions($options));
+        ], $this->_resolveDestOptions($params));
+
+        return $destBody;
     }
 
-    private function _resolveSourceOptions(array $options)
+    private function _resolveSourceOptions(array $params): array
     {
-        return \array_intersect_key($options, [
+        return \array_intersect_key($params, [
             self::TYPE => null,
             self::QUERY => null,
+            self::SORT => null,
+            self::SOURCE => null,
+            self::REMOTE => null,
+            self::SLICE => null,
         ]);
     }
 
-    private function _resolveDestOptions(array $options)
+    private function _resolveDestOptions(array $params): array
     {
-        return \array_intersect_key($options, [
+        return \array_intersect_key($params, [
             self::VERSION_TYPE => null,
             self::OPERATION_TYPE => null,
         ]);
     }
 
-    private function _resolveBodyOptions(array $options)
+    private function _resolveBodyOptions(array $params): array
     {
-        return \array_intersect_key($options, [
+        return \array_intersect_key($params, [
             self::SIZE => null,
             self::CONFLICTS => null,
         ]);
     }
 
-    /**
-     * @param array $sourceBody
-     *
-     * @return array
-     */
-    private function _setSourceQuery(array $sourceBody)
+    private function _setSourceQuery(array $sourceBody): array
     {
         if (isset($sourceBody[self::QUERY]) && $sourceBody[self::QUERY] instanceof AbstractQuery) {
             $sourceBody[self::QUERY] = $sourceBody[self::QUERY]->toArray();
@@ -119,12 +143,7 @@ class Reindex
         return $sourceBody;
     }
 
-    /**
-     * @param array $sourceBody
-     *
-     * @return array
-     */
-    private function _setSourceType(array $sourceBody)
+    private function _setSourceType(array $sourceBody): array
     {
         if (isset($sourceBody[self::TYPE]) && !\is_array($sourceBody[self::TYPE])) {
             $sourceBody[self::TYPE] = [$sourceBody[self::TYPE]];
@@ -138,5 +157,64 @@ class Reindex
         }
 
         return $sourceBody;
+    }
+
+    private function _setBodyScript(array $body): array
+    {
+        if (!$this->hasParam(self::SCRIPT)) {
+            return $body;
+        }
+
+        $script = $this->getParam(self::SCRIPT);
+
+        if ($script instanceof AbstractScript) {
+            $body = \array_merge($body, $script->toArray());
+        } else {
+            $body[self::SCRIPT] = $script;
+        }
+
+        return $body;
+    }
+
+    public function setWaitForCompletion($value)
+    {
+        \is_bool($value) && $value = $value ? 'true' : 'false';
+
+        $this->setParam(self::WAIT_FOR_COMPLETION, $value);
+    }
+
+    public function setWaitForActiveShards($value)
+    {
+        $this->setParam(self::WAIT_FOR_ACTIVE_SHARDS, $value);
+    }
+
+    public function setTimeout($value)
+    {
+        $this->setParam(self::TIMEOUT, $value);
+    }
+
+    public function setScroll($value)
+    {
+        $this->setParam(self::SCROLL, $value);
+    }
+
+    public function setRequestsPerSecond($value)
+    {
+        $this->setParam(self::REQUESTS_PER_SECOND, $value);
+    }
+
+    public function setScript(Script $script)
+    {
+        $this->setParam(self::SCRIPT, $script);
+    }
+
+    public function getTaskId()
+    {
+        $taskId = null;
+        if ($this->_lastResponse instanceof Response) {
+            $taskId = $this->_lastResponse->getData()['task'] ? $this->_lastResponse->getData()['task'] : null;
+        }
+
+        return $taskId;
     }
 }
