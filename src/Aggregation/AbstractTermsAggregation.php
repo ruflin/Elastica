5dacf3ecedb37fb2156537ec535ebd86f11b0748
François-Xavier de Guillebon <deguif@gmail.com>
2020-10-29 13:27:30 +0100

    Fix return type-hints for better chainability

--- src/Aggregation/AbstractTermsAggregation.php
+++ src/Aggregation/AbstractTermsAggregation.php
@@ -12,6 +12,8 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
 
     /**
      * Set the minimum number of documents in which a term must appear in order to be returned in a bucket.
+     *
+     * @return $this
      */
     public function setMinimumDocumentCount(int $count): self
     {
@@ -24,6 +26,8 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
      * @See https://www.elastic.co/guide/en/elasticsearch/reference/current/regexp-syntax.html for syntax
      *
      * @param string $pattern a regular expression, following the Regexp syntax
+     *
+     * @return $this
      */
     public function setInclude(string $pattern): self
     {
@@ -33,9 +37,11 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
     /**
      * Filter documents to include based on a list of exact values.
      *
+     * @See https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html#_filtering_values_with_exact_values_2
+     *
      * @param string[] $values
      *
-     * @See https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html#_filtering_values_with_exact_values_2
+     * @return $this
      */
     public function setIncludeAsExactMatch(array $values): self
     {
@@ -46,6 +52,8 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
      * Set the aggregation filter to use partitions.
      *
      * @See https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html#_filtering_values_with_partitions
+     *
+     * @return $this
      */
     public function setIncludeWithPartitions(int $partition, int $numPartitions): self
     {
@@ -59,6 +67,8 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
      * Filter documents to exclude based on a regular expression.
      *
      * @param string $pattern a regular expression
+     *
+     * @return $this
      */
     public function setExclude(string $pattern): self
     {
@@ -68,9 +78,11 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
     /**
      * Filter documents to exclude based on a list of exact values.
      *
+     * @See https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html#_filtering_values_with_exact_values_2
+     *
      * @param string[] $values
      *
-     * @See https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html#_filtering_values_with_exact_values_2
+     * @return $this
      */
     public function setExcludeAsExactMatch(array $values): self
     {
@@ -79,6 +91,8 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
 
     /**
      * Sets the amount of terms to be returned.
+     *
+     * @return $this
      */
     public function setSize(int $size): self
     {
@@ -87,6 +101,8 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
 
     /**
      * Sets how many terms the coordinating node will request from each shard.
+     *
+     * @return $this
      */
     public function setShardSize(int $shardSize): self
     {
@@ -100,6 +116,8 @@ abstract class AbstractTermsAggregation extends AbstractSimpleAggregation
      * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html#search-aggregations-bucket-terms-aggregation-execution-hint
      *
      * @param string $hint Execution hint, use one of self::EXECUTION_HINT_MAP or self::EXECUTION_HINT_GLOBAL_ORDINALS
+     *
+     * @return $this
      */
     public function setExecutionHint(string $hint): self
     {
