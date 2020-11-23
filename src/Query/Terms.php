ruflin/Elastica

commit a3234502d2b80e872a9762c0477d32b01fcf2bb5

Allow the Terms query to accept arrays of strings, ints and floats (#1872)
As shown in https://github.com/ruflin/Elastica/pull/1765#discussion_r525927129 , ElasticSearch accepts all of them

Note: the change of `public function addTerm` was not previously discussed, but is made for consistency, however poses a breaking change if someone extended this class previously => https://3v4l.org/Ws5bM

src/Query/Terms.php
@@ -20,7 +20,7 @@ class Terms extends AbstractQuery
     private $field;
 
     /**
-     * @var string[]
+     * @var array<float|int|string>
      */
     private $terms;
 
@@ -30,7 +30,7 @@ class Terms extends AbstractQuery
     private $lookup;
 
     /**
-     * @param string[] $terms Terms list, leave empty if building a terms-lookup query
+     * @param array<float|int|string> $terms Terms list, leave empty if building a terms-lookup query
      */
     public function __construct(string $field, array $terms = [])
     {
@@ -45,7 +45,7 @@ class Terms extends AbstractQuery
     /**
      * Sets terms for the query.
      *
-     * @param string[]
+     * @param array<float|int|string>
      */
     public function setTerms(array $terms): self
     {
@@ -56,9 +56,15 @@ class Terms extends AbstractQuery
 
     /**
      * Adds a single term to the list.
+     *
+     * @param float|int|string $term
      */
-    public function addTerm(string $term): self
+    public function addTerm($term): self
     {
+        if (!\is_string($term) && !\is_int($term) && !\is_float($term)) {
+            throw new \TypeError(\sprintf('Argument 1 passed to "%s()" must be of type float|int|string, %s given.', __METHOD__, \is_object($term) ? \get_class($term) : \gettype($term)));
+        }
+
         $this->terms[] = $term;
 
         return $this;

tests/Query/TermsTest.php
@@ -96,4 +96,92 @@ class TermsTest extends BaseTest
 
         $this->assertEquals(2, $resultSet->count());
     }
+
+    /**
+     * @group functional
+     */
+    public function testVariousDataTypesViaConstructor(): void
+    {
+        $index = $this->_createIndex();
+
+        $index->addDocuments([
+            new Document(1, ['some_numeric_field' => 9876]),
+        ]);
+        $index->refresh();
+
+        // string
+        $query = new Terms('some_numeric_field', ['9876']);
+        $resultSet = $index->search($query);
+        $this->assertEquals(1, $resultSet->count());
+
+        // int
+        $query = new Terms('some_numeric_field', [9876]);
+        $resultSet = $index->search($query);
+        $this->assertEquals(1, $resultSet->count());
+
+        // float
+        $query = new Terms('some_numeric_field', [9876.0]);
+        $resultSet = $index->search($query);
+        $this->assertEquals(1, $resultSet->count());
+    }
+
+    /**
+     * @group functional
+     */
+    public function testVariousMixedDataTypesViaConstructor(): void
+    {
+        $index = $this->_createIndex();
+
+        $index->addDocuments([
+            new Document(1, ['some_numeric_field' => 9876]),
+            new Document(2, ['some_numeric_field' => 5678]),
+            new Document(3, ['some_numeric_field' => 1234]),
+            new Document(4, ['some_numeric_field' => 8899]),
+        ]);
+        $index->refresh();
+
+        $query = new Terms('some_numeric_field', ['9876', 1234, 5678.0]);
+        $resultSet = $index->search($query);
+        $this->assertEquals(3, $resultSet->count());
+    }
+
+    /**
+     * @group functional
+     */
+    public function testVariousDataTypesViaAddTerm(): void
+    {
+        $index = $this->_createIndex();
+
+        $index->addDocuments([
+            new Document(1, ['some_numeric_field' => 9876]),
+        ]);
+        $index->refresh();
+
+        // string
+        $query = new Terms('some_numeric_field');
+        $query->addTerm('9876');
+        $resultSet = $index->search($query);
+        $this->assertEquals(1, $resultSet->count());
+
+        // int
+        $query = new Terms('some_numeric_field');
+        $query->addTerm(9876);
+        $resultSet = $index->search($query);
+        $this->assertEquals(1, $resultSet->count());
+
+        // float
+        $query = new Terms('some_numeric_field');
+        $query->addTerm(9876.0);
+        $resultSet = $index->search($query);
+        $this->assertEquals(1, $resultSet->count());
+    }
+
+    public function testAddTermTypeError(): void
+    {
+        $this->expectException(\TypeError::class);
+        $this->expectExceptionMessage('Argument 1 passed to "Elastica\Query\Terms::addTerm()" must be of type float|int|string, stdClass given.');
+
+        $query = new Terms('some_numeric_field');
+        $query->addTerm(new \stdClass());
+    }
 }

commit a7e387e0a020dc3c330eae8cf619713c863ed97c

Fix regression introduced in #1872

src/Query/Terms.php
@@ -30,7 +30,7 @@ class Terms extends AbstractQuery
     private $lookup;
 
     /**
-     * @param array<float|int|string> $terms Terms list, leave empty if building a terms-lookup query
+     * @param array<bool|float|int|string> $terms Terms list, leave empty if building a terms-lookup query
      */
     public function __construct(string $field, array $terms = [])
     {
@@ -45,7 +45,7 @@ class Terms extends AbstractQuery
     /**
      * Sets terms for the query.
      *
-     * @param array<float|int|string>
+     * @param array<bool|float|int|string>
      */
     public function setTerms(array $terms): self
     {
@@ -57,12 +57,12 @@ class Terms extends AbstractQuery
     /**
      * Adds a single term to the list.
      *
-     * @param float|int|string $term
+     * @param bool|float|int|string $term
      */
     public function addTerm($term): self
     {
-        if (!\is_string($term) && !\is_int($term) && !\is_float($term)) {
-            throw new \TypeError(\sprintf('Argument 1 passed to "%s()" must be of type float|int|string, %s given.', __METHOD__, \is_object($term) ? \get_class($term) : \gettype($term)));
+        if (!\is_scalar($term)) {
+            throw new \TypeError(\sprintf('Argument 1 passed to "%s()" must be a scalar, %s given.', __METHOD__, \is_object($term) ? \get_class($term) : \gettype($term)));
         }
 
         $this->terms[] = $term;

tests/Query/TermsTest.php
@@ -179,7 +179,7 @@ class TermsTest extends BaseTest
     public function testAddTermTypeError(): void
     {
         $this->expectException(\TypeError::class);
-        $this->expectExceptionMessage('Argument 1 passed to "Elastica\Query\Terms::addTerm()" must be of type float|int|string, stdClass given.');
+        $this->expectExceptionMessage('Argument 1 passed to "Elastica\Query\Terms::addTerm()" must be a scalar, stdClass given.');
 
         $query = new Terms('some_numeric_field');
         $query->addTerm(new \stdClass());