aa3205ed46979ab300cba1f4ee7030c75bef8d7b
François-Xavier de Guillebon <deguif@gmail.com>
2020-12-03 16:34:24 +0100

    Review handling of processor base name

--- src/Processor/AbstractProcessor.php
+++ src/Processor/AbstractProcessor.php
@@ -3,6 +3,7 @@
 namespace Elastica\Processor;
 
 use Elastica\Param;
+use Elastica\Util;
 
 /**
  * Elastica Processor object.
@@ -13,4 +14,11 @@ use Elastica\Param;
  */
 abstract class AbstractProcessor extends Param
 {
+    protected function _getBaseName()
+    {
+        $shortName = (new \ReflectionClass($this))->getShortName();
+        $shortName = \preg_replace('/Processor$/', '', $shortName);
+
+        return Util::toSnakeCase($shortName);
+    }
 }
