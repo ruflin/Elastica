c78cf2a9bf3cbc0a8f766296da617abda37e2608
François-Xavier de Guillebon <deguif@gmail.com>
2021-11-17 12:08:23 +0100

    Deprecate multi_match query cutoff frequency option (#2015)
    * Deprecate multi_match query cutoff_frequency option
    
    * Add changelog

--- src/Query/MultiMatch.php
+++ src/Query/MultiMatch.php
@@ -115,9 +115,13 @@ class MultiMatch extends AbstractQuery
      * Set cutoff frequency for Match Query.
      *
      * @return $this
+     *
+     * @deprecated since 7.1.3
      */
     public function setCutoffFrequency(float $cutoffFrequency): self
     {
+        \trigger_deprecation('ruflin/elastica', '7.1.3', 'The "%s()" method is deprecated. It will be removed in 8.0.', __METHOD__);
+
         return $this->setParam('cutoff_frequency', $cutoffFrequency);
     }
 
