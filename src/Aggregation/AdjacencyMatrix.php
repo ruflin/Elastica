fa33d6c323b279e497adad3ef2f3479b258b8c03
François-Xavier de Guillebon <deguif@gmail.com>
2021-11-30 17:18:25 +0100

    Allow to configure a subkey when adding a param

--- src/Aggregation/AdjacencyMatrix.php
+++ src/Aggregation/AdjacencyMatrix.php
@@ -13,10 +13,7 @@ class AdjacencyMatrix extends AbstractAggregation
      */
     public function addFilter(AbstractQuery $filter, string $name): self
     {
-        $filterArray = [];
-        $filterArray[$name] = $filter;
-
-        return $this->addParam('filters', $filterArray);
+        return $this->addParam('filters', $filter, $name);
     }
 
     /**
@@ -25,26 +22,5 @@ class AdjacencyMatrix extends AbstractAggregation
     public function setSeparator(string $separator = '&'): self
     {
         return $this->setParam('separator', $separator);
-    }
-
-    public function toArray(): array
-    {
-        $array = [];
-        $filters = $this->getParam('filters');
-
-        foreach ($filters as $filter) {
-            $key = \key($filter);
-            $array['adjacency_matrix']['filters'][$key] = \current($filter)->toArray();
-        }
-
-        if ($this->hasParam('separator')) {
-            $array['adjacency_matrix']['separator'] = $this->getParam('separator');
-        }
-
-        if ($this->_aggs) {
-            $array['aggs'] = $this->_convertArrayable($this->_aggs);
-        }
-
-        return $array;
     }
 }
