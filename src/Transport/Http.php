3bcd0a6067abc581bf3cbfdbe22e96d4c6daaaca
Fran Moreno <franmomu@gmail.com>
2022-05-24 10:37:24 +0200

    Remove "0" as valid request data for the _analyze endopoint (#2068)

--- src/Transport/Http.php
+++ src/Transport/Http.php
@@ -122,7 +122,7 @@ class Http extends AbstractTransport
 
         $headers[] = 'Content-Type: '.$request->getContentType();
 
-        if (!empty($data) || '0' === $data) {
+        if (!empty($data)) {
             if ($this->hasParam('postWithRequestBody') && true == $this->getParam('postWithRequestBody')) {
                 $httpMethod = Request::POST;
             }
