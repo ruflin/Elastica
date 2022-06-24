591e3b963914551aa494f2d62428b4f70a75b02b
Fran Moreno <franmomu@gmail.com>
2022-06-24 12:44:37 +0200

    Allow int and float for FunctionScore::addDecayFunction() origin and scale

--- src/Query/FunctionScore.php
+++ src/Query/FunctionScore.php
@@ -108,12 +108,14 @@ class FunctionScore extends AbstractQuery
     }
 
     /**
+     * TODO: Change "$origin" and "$scale" parameter types to allow "float|int|string".
+     *
      * Add a decay function to the query.
      *
      * @param string             $function       see DECAY_* constants for valid options
      * @param string             $field          the document field on which to perform the decay function
-     * @param string             $origin         the origin value for this decay function
-     * @param string             $scale          a scale to define the rate of decay for this function
+     * @param float|int|string   $origin         the origin value for this decay function
+     * @param float|int|string   $scale          a scale to define the rate of decay for this function
      * @param string|null        $offset         If defined, this function will only be computed for documents with a distance from the origin greater than this value
      * @param float|null         $decay          optionally defines how documents are scored at the distance given by the $scale parameter
      * @param float|null         $weight         optional factor by which to multiply the score at the value provided by the $scale parameter
