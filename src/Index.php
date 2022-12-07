a7d1e7bb73bc052ec7a13f723cc5a2b40a69951f
François-Xavier de Guillebon <deguif@gmail.com>
2022-12-07 15:33:07 +0100

    Remove deprecated index creation options

--- src/Index.php
+++ src/Index.php
@@ -406,20 +406,8 @@ class Index implements SearchableInterface
      *
      * @return Response Server response
      */
-    public function create(array $args = [], $options = null): Response
+    public function create(array $args = [], array $options = []): Response
     {
-        if (null === $options) {
-            if (\func_num_args() >= 2) {
-                \trigger_deprecation('ruflin/elastica', '7.1.0', 'Passing null as 2nd argument to "%s()" is deprecated, avoid passing this argument or pass an array instead. It will be removed in 8.0.', __METHOD__);
-            }
-            $options = [];
-        } elseif (\is_bool($options)) {
-            \trigger_deprecation('ruflin/elastica', '7.1.0', 'Passing a bool as 2nd argument to "%s()" is deprecated, pass an array with the key "recreate" instead. It will be removed in 8.0.', __METHOD__);
-            $options = ['recreate' => $options];
-        } elseif (!\is_array($options)) {
-            throw new \TypeError(\sprintf('Argument 2 passed to "%s()" must be of type array|bool|null, %s given.', __METHOD__, \is_object($options) ? \get_class($options) : \gettype($options)));
-        }
-
         $endpoint = new Create();
         $invalidOptions = \array_diff(\array_keys($options), $allowedOptions = \array_merge($endpoint->getParamWhitelist(), [
             'recreate',
