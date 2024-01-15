2ffcaa9fd34bd91ed0d93c3308eef4185c085153
Paweł Kęska <projekty@pawelkeska.eu>
2024-01-15 11:31:06 +0100

    [Tests] Fix assertObjectHasProperty

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -253,7 +253,7 @@ class DocumentTest extends BaseTest
         }
 
         $this->assertEquals('changed1', $document->field1);
-        $this->assertObjectNotHasProperty('field3', $document);
+        $this->assertFalse(property_exists($document, 'field3'));
 
         $newData = $document->getData();
 
