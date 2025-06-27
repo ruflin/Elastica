6266e9d9a9366d9e9bc035a5f92bd541b075086d
Tito Miguel Costa <tito.costa@speakap.nl>
2025-06-27 20:12:37 +0200

    change test to accept margin of error on geo coordinates

--- test/Elastica/Aggregation/GeoBoundsTest.php
+++ test/Elastica/Aggregation/GeoBoundsTest.php
@@ -40,9 +40,9 @@ class GeoBoundsTest extends BaseAggregationTest
         $query->addAggregation($agg);
         $results = $this->getIndexForTest()->search($query)->getAggregation('viewport');
 
-        $this->assertEquals(37.782438984140754, $results['bounds']['top_left']['lat']);
-        $this->assertEquals(-122.39256000146, $results['bounds']['top_left']['lon']);
-        $this->assertEquals(32.798319971189, $results['bounds']['bottom_right']['lat']);
-        $this->assertEquals(-117.24664804526, $results['bounds']['bottom_right']['lon']);
+        $this->assertEqualsWithDelta(37.782438984140754, $results['bounds']['top_left']['lat'], 0.0001);
+        $this->assertEqualsWithDelta(-122.39256000146, $results['bounds']['top_left']['lon'], 0.0001);
+        $this->assertEqualsWithDelta(32.798319971189, $results['bounds']['bottom_right']['lat'], 0.0001);
+        $this->assertEqualsWithDelta(-117.24664804526, $results['bounds']['bottom_right']['lon'], 0.0001);
     }
 }
