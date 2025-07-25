5ec425b453ebb3bea17e291defb03d257d6e5105
nyro <cedric@nyro.dev>
2025-07-25 23:35:59 +0200

    Use correct indexTemplate api call for non legacy usages

--- src/IndexTemplate.php
+++ src/IndexTemplate.php
@@ -86,7 +86,7 @@ class IndexTemplate
         }
 
         return $this->_client->toElasticaResponse(
-            $this->_client->indices()->deleteTemplate(['name' => $this->getName()])
+            $this->_client->indices()->deleteIndexTemplate(['name' => $this->getName()])
         );
     }
 
@@ -110,7 +110,7 @@ class IndexTemplate
         }
 
         return $this->_client->toElasticaResponse(
-            $this->_client->indices()->putTemplate(['name' => $this->getName(), 'body' => $args])
+            $this->_client->indices()->putIndexTemplate(['name' => $this->getName(), 'body' => $args])
         );
     }
 
@@ -128,7 +128,7 @@ class IndexTemplate
         if ($this->_useLegacy) {
             return $this->_legacyTemplate->exists();
         }
-        $response = $this->_client->indices()->existsTemplate(['name' => $this->getName()]);
+        $response = $this->_client->indices()->existsIndexTemplate(['name' => $this->getName()]);
 
         return 200 === $response->getStatusCode();
     }
