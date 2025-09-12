0ba92ce43991f9a90c1fe56646620612ea57ba32
hotrush <johny64@gmail.com>
2025-09-12 13:31:19 +0200

    Allow zero retries number

--- src/Client.php
+++ src/Client.php
@@ -589,7 +589,7 @@ class Client implements ClientInterface
         $transport = $builder->build();
 
         // The default retries is equal to the number of hosts
-        if (isset($config['retries']) && (int) $config['retries'] > 0) {
+        if (isset($config['retries']) && (int) $config['retries'] >= 0) {
             $transport->setRetries($config['retries']);
         } else {
             $transport->setRetries(\count($hosts));
